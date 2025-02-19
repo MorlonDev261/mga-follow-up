e511f2368c8f4c48b6e9688c36d76bf281f99178
MorlonDev261 <mikandrapp@gmail.com>
2025-02-19 23:15:06 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+"use client";
+
+import { useState } from "react";
 import Header from "@components/Header";
 import Sidebar from "@components/Sidebar";
 
 export default function Home() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   return (
     <>
-      <Header />
-      <Sidebar />
+      <Header isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} />
+      <Sidebar isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} />
     </>
   );
 }
