9d3dcf08d21598dca371da68e2960bd5ff16f872
MorlonDev261 <mikandrapp@gmail.com>
2025-02-20 02:27:33 +0300

    Update index.tsx

--- src/app/_components/Sidebar/index.tsx
+++ src/app/_components/Sidebar/index.tsx
@@ -1,10 +1,4 @@
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-} from "@/components/ui/sheet"
+import { FaTimes } from 'react-icons/fa';
 import "./Sidebar.css";
 
 interface SidebarProps {
@@ -15,20 +9,39 @@ interface SidebarProps {
 
 const Sidebar = ({ sidebarState, setSidebarState }: SidebarProps) => {
   return (
-    <aside className="sidebar">
-      <Sheet open={sidebarState} onOpenChange={setSidebarState}>
-        <SheetContent 
-          side="left"
-          className="data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left">
-          <SheetHeader>
-            <SheetTitle>Are you absolutely sure?</SheetTitle>
-            <SheetDescription>
-              This action cannot be undone. This will permanently delete your account
-              and remove your data from our servers.
-            </SheetDescription>
-          </SheetHeader>
-        </SheetContent>
-      </Sheet>
+    <aside className={`sidebar ${sidebarState ? "active" : ""}`}>
+      <div className="nav-section p-5 nav-left">
+          <Link href="/" className="nav-logo">
+              <Image src="/icons/logo-aztek.svg" alt="Logo" width={50} height={50} className="logo-image" />
+          </Link>
+          <button className="nav-button theme-button" onClick={() => setSidebarState(false)}>
+            <FaTimes />
+          </button>
+        </div>
+      
+        <div className="link-container">
+          {/* Section principale */}
+          <div className="link-section">
+            <Link href="/products" className="link-item change-type">
+              <Image className="profile rounded-full" src="/users/profile-1.jpg" width="30" height="30" alt="ShopCell" /> 
+              <span>Shop Cell</span>
+              <small>Last updated: 12 mai</small>
+            </Link>
+            <Link href="/products" className="link-item change-type">
+              <Image className="profile" src="/users/profile-1.jpg" width="30" height="30" alt="ShopCell" /> 
+              <span>Shop Cell</span>
+              <small>Last updated: 12 mai</small>
+            </Link>
+            <Link href="/products" className="link-item change-type">
+              <Image className="profile" src="/users/profile-1.jpg" width="30" height="30" alt="ShopCell" /> 
+              <span>Shop Cell</span>
+              <small>Last updated: 12 mai</small>
+            </Link>
+          </div>
+
+
+          <div className="section-separator" />
+        </div>
     </aside>
   );
 };
