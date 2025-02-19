abc393cafc8d21a92c8ca516be0d62076efd302e
MorlonDev261 <mikandrapp@gmail.com>
2025-02-19 20:07:03 +0300

    Update index.tsx

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -1,1 +1,33 @@
+'use client';
 
+import Link from 'next/link';
+import './Header.css';
+import { FaRegEnvelope } from "react-icons/fa6";
+import { FaBars, FaRegBell } from 'react-icons/fa';
+
+const Header () {
+  return (
+    <header>
+      <nav className="navbar">
+        <div className="nav-section">
+          <button sidebar-type="sidebar-left" className="nav-button menu-button">
+            <FaBars className="icon" />
+          </button>
+        </div>
+        
+        <div className="nav-section">
+          <Link href="#" className="nav-button message-button">
+            <FaRegEnvelope className="icon" />
+            <span className="count">5</span>
+          </Link>
+          <button className="nav-button notif-button">
+            <FaRegBell className="icon" />
+            <span className="count">1</span>
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
