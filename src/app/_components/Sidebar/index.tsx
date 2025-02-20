5347aa5a2a366a0e7cfa7db204c5f5934063fd74
MorlonDev261 <mikandrapp@gmail.com>
2025-02-20 08:09:55 +0300

    Update index.tsx

--- src/app/_components/Sidebar/index.tsx
+++ src/app/_components/Sidebar/index.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { FaTimes } from "react-icons/fa";
-import "./Sidebar.css";
+import "./CSS/Sidebar.css";
 
 interface SidebarProps {
   sidebarState: boolean;
