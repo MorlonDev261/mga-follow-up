ec1970e83c452459d1d8654c28c6e73a43ef3159
MorlonDev261 <mikandrapp@gmail.com>
2025-02-20 19:43:25 +0300

    Update index.tsx

--- src/app/_components/ShortcutList/index.tsx
+++ src/app/_components/ShortcutList/index.tsx
@@ -3,24 +3,29 @@ import { FaDollarSign, FaCalendarAlt, FaBox, FaUser, FaCog } from 'react-icons/f
 import { FiDollarSign, FiClock, FiUsers, FiUser } from 'react-icons/fi';
 
 const shortcuts = [
-  { label: 'Revenu', icon: <FaDollarSign /> }, // Icône de dollar pour le revenu
-  { label: 'Depense', icon: <FiDollarSign /> }, // Icône de dollar pour la dépense
-  { label: 'Pending', icon: <FiClock /> }, // Icône d'horloge pour "Pending"
-  { label: 'Agenda', icon: <FaCalendarAlt /> }, // Icône de calendrier pour l'agenda
-  { label: 'Stock', icon: <FaBox /> }, // Icône de boîte pour le stock
-  { label: 'Client', icon: <FiUsers /> }, // Icône d'utilisateurs pour les clients
-  { label: 'Profile', icon: <FiUser /> }, // Icône d'utilisateur pour le profil
-  { label: 'Paramètre', icon: <FaCog /> }, // Icône d'engrenage pour les paramètres
+  { label: 'Revenu', icon: <FaDollarSign /> },
+  { label: 'Depense', icon: <FiDollarSign /> },
+  { label: 'Pending', icon: <FiClock /> },
+  { label: 'Agenda', icon: <FaCalendarAlt /> },
+  { label: 'Stock', icon: <FaBox /> },
+  { label: 'Client', icon: <FiUsers /> },
+  { label: 'Profile', icon: <FiUser /> },
+  { label: 'Paramètre', icon: <FaCog /> },
 ];
 
 const ShortcutList = () => {
   return (
-    {shortcuts.map((shortcut, index) => (
-      <div key={index} className="flex h-20 flex-col items-center justify-center rounded bg-[#222] p-2">
-        <span className="mb-1 text-xl">{shortcut.icon}</span>
-        <span className="text-xs">{shortcut.label}</span>
-      </div>
-    ))}
+    <>
+      {shortcuts.map((shortcut) => (
+        <div
+          key={shortcut.label}
+          className="flex h-20 flex-col items-center justify-center rounded bg-[#222] p-2"
+        >
+          <span className="mb-1 text-xl">{shortcut.icon}</span>
+          <span className="text-xs">{shortcut.label}</span>
+        </div>
+      ))}
+    </>
   );
 };
 
