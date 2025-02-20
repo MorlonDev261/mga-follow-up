4664ef28065073bbbdf7529a01e916ac5424014c
MorlonDev261 <mikandrapp@gmail.com>
2025-02-21 02:51:44 +0300

    Update index.tsx

--- src/app/_components/Dashboard/index.tsx
+++ src/app/_components/Dashboard/index.tsx
@@ -69,7 +69,7 @@ export default function Dashboard() {
                 </div>
                 <p className="text-gray-400">= 4350 AIED</p>
               </div>
-              <div className="mb-5 max-h-[100px] mr-auto">
+              <div className="mb-5 max-h-[100px] bg-gray-500">
                 <Statistique position="right" />
               </div>
               <div className="grid grid-cols-3 gap-2 sm:grid-cols-4 lg:grid-cols-4">
