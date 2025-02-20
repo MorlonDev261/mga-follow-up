86af0de62c096f75c9d46285ca286a3a72c9c4d5
MorlonDev261 <mikandrapp@gmail.com>
2025-02-20 08:06:44 +0300

    Update index.tsx

--- src/app/_components/Sidebar/index.tsx
+++ src/app/_components/Sidebar/index.tsx
@@ -1,50 +1,52 @@
 import Link from "next/link";
 import Image from "next/image";
-import { FaTimes } from 'react-icons/fa';
+import { FaTimes } from "react-icons/fa";
 import "./Sidebar.css";
 
 interface SidebarProps {
   sidebarState: boolean;
   setSidebarState: React.Dispatch<React.SetStateAction<boolean>>;
-
 }
 
 const Sidebar = ({ sidebarState, setSidebarState }: SidebarProps) => {
   return (
-    <aside className={`sidebar bg-white ${sidebarState ? "active" : ""}`}>
-      <div className="nav-section p-5 nav-left">
+    <>
+      {/* Overlay */}
+      <div className={`overlay ${sidebarState ? "active" : ""}`} onClick={() => setSidebarState(false)}></div>
+
+      <aside className={`sidebar bg-white ${sidebarState ? "active" : ""}`}>
+        <div className="nav-section p-5 nav-left">
           <Link href="/" className="nav-logo">
-              <Image src="/icons/logo-aztek.svg" alt="Logo" width={50} height={50} className="logo-image" />
+            <Image src="/icons/logo-aztek.svg" alt="Logo" width={50} height={50} className="logo-image" />
           </Link>
           <button className="nav-button theme-button" onClick={() => setSidebarState(false)}>
             <FaTimes />
           </button>
         </div>
-      
+
         <div className="link-container">
-          {/* Section principale */}
           <div className="link-section">
             <Link href="/products" className="link-item change-type">
-              <Image className="profile rounded-full" src="/users/profile-1.jpg" width="30" height="30" alt="ShopCell" /> 
+              <Image className="profile rounded-full" src="/users/profile-1.jpg" width="30" height="30" alt="ShopCell" />
               <span>Shop Cell</span>
               <small>Last updated: 12 mai</small>
             </Link>
             <Link href="/products" className="link-item change-type">
-              <Image className="profile" src="/users/profile-1.jpg" width="30" height="30" alt="ShopCell" /> 
+              <Image className="profile" src="/users/profile-1.jpg" width="30" height="30" alt="ShopCell" />
               <span>Shop Cell</span>
               <small>Last updated: 12 mai</small>
             </Link>
             <Link href="/products" className="link-item change-type">
-              <Image className="profile" src="/users/profile-1.jpg" width="30" height="30" alt="ShopCell" /> 
+              <Image className="profile" src="/users/profile-1.jpg" width="30" height="30" alt="ShopCell" />
               <span>Shop Cell</span>
               <small>Last updated: 12 mai</small>
             </Link>
           </div>
 
-
           <div className="section-separator" />
         </div>
-    </aside>
+      </aside>
+    </>
   );
 };
 
