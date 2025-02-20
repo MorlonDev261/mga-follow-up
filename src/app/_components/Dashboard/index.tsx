79a09f2fb7a8af2c3fb071f1a6e9152983c32414
MorlonDev261 <mikandrapp@gmail.com>
2025-02-21 02:18:21 +0300

    Update index.tsx

--- src/app/_components/Dashboard/index.tsx
+++ src/app/_components/Dashboard/index.tsx
@@ -69,7 +69,7 @@ export default function Dashboard() {
                 </div>
                 <p className="text-gray-400">= 0.00151512 BTC</p>
               </div>
-              <div className="mb-5 max-h-[100px] w-full">
+              <div className="mb-5 flex max-h-[100px] w-full">
                 <Statistique position="right" />
               </div>
               <div className="grid grid-cols-3 gap-2 sm:grid-cols-4 lg:grid-cols-4">
