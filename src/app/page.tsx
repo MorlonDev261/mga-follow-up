266447dfbe198219c96496b0c1a2f0a4fa6a0536
MorlonDev261 <mikandrapp@gmail.com>
2025-02-20 19:04:33 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,10 @@
 "use client";
 
-import { useState } from "react";
-import Header from "@components/Header";
-import Sidebar from "@components/Sidebar";
+import Home from "@components/Home";
 
 export default function Home() {
-  const [sidebarState, setSidebarState] = useState(false);
 
   return (
-    <>
-      <Header sidebarState={sidebarState} setSidebarState={setSidebarState} />
-      <Sidebar sidebarState={sidebarState} setSidebarState={setSidebarState} />
-    </>
+      <Home />
   );
 }
