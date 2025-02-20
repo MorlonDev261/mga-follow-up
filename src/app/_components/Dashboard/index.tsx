c8562f61284aa4dab494af0c8115a73a371d663f
MorlonDev261 <mikandrapp@gmail.com>
2025-02-20 21:23:22 +0300

    Update index.tsx

--- src/app/_components/Dashboard/index.tsx
+++ src/app/_components/Dashboard/index.tsx
@@ -68,7 +68,9 @@ export default function Dashboard() {
                 </div>
                 <p className="text-gray-400">= 0.00151512 BTC</p>
               </div>
-
+              <div className="grid grid-cols-2 gap-2 sm:grid-cols-4 lg:grid-cols-4">
+                <ShortcutList />
+              </div>
               <div className="grid grid-cols-2 gap-2 sm:grid-cols-4 lg:grid-cols-4">
                 <ShortcutList />
               </div>
