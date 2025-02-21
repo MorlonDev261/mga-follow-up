c1bab0cd09d3a0bf26374acc326dd0bcb7d82b5a
MorlonDev261 <mikandrapp@gmail.com>
2025-02-21 19:23:26 +0300

    Update index.tsx

--- src/app/_components/ExcelTab/index.tsx
+++ src/app/_components/ExcelTab/index.tsx
@@ -1,4 +1,5 @@
 import { HotTable } from '@handsontable/react';
+import type { HotTableClass } from '@handsontable/react';
 import Handsontable from 'handsontable';
 import 'handsontable/dist/handsontable.full.min.css';
 import { useState, useRef } from 'react';
@@ -14,7 +15,7 @@ interface FinancialDataRow {
 }
 
 const ExcelTab = () => {
-  const hotTableRef = useRef<typeof HotTable | null>(null);
+  const hotTableRef = useRef<HotTableClass | null>(null);
   const [data, setData] = useState<FinancialDataRow[]>([
     { date: '2024-01-01', client: 'Client A', income: 500000, expenses: 200000, comments: 'Commentaire 1', net: 300000 },
     { date: '2024-01-02', client: 'Client B', income: 750000, expenses: 300000, comments: 'Commentaire 2', net: 450000 },
