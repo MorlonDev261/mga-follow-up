ed1ed2d8fbb03dbadfcd606237ed4537fcfd9eac
MorlonDev261 <mikandrapp@gmail.com>
2025-02-23 16:03:30 +0300

    Update and rename DropdownMenu.tsx to index.tsx

--- src/app/_components/DropdownComponent/index.tsx
+++ src/app/_components/DropdownComponent/index.tsx
@@ -15,7 +15,7 @@ import {
 
 type Checked = DropdownMenuCheckboxItemProps["checked"]
 
-export function DropdownMenu() {
+export function DropdownComponent() {
   const [showStatusBar, setShowStatusBar] = React.useState<Checked>(true)
   const [showActivityBar, setShowActivityBar] = React.useState<Checked>(false)
   const [showPanel, setShowPanel] = React.useState<Checked>(false)
