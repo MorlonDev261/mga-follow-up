adb551b98060f0f96915f0129ca4bb165b1ddabf
MorlonDev261 <mikandrapp@gmail.com>
2025-02-23 21:37:15 +0300

    Update textfield.tsx

--- src/components/ui/textfield.tsx
+++ src/components/ui/textfield.tsx
@@ -3,22 +3,20 @@
 import * as React from "react";
 import { cn } from "@/lib/utils";
 
-export interface TextFieldProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
-
-export const TextField = React.forwardRef<HTMLInputElement, TextFieldProps>(
-  ({ className, ...props }, ref) => {
-    return (
-      <input
-        ref={ref}
-        className={cn(
-          "w-full rounded-lg border border-gray-300 px-3 py-2 text-sm focus:border-blue-500 focus:ring-1 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:focus:border-blue-400 dark:focus:ring-blue-400",
-          className
-        )}
-        {...props}
-      />
-    );
-  }
-);
+export const TextField = React.forwardRef<
+  HTMLInputElement,
+  React.InputHTMLAttributes<HTMLInputElement>
+>(({ className, ...props }, ref) => {
+  return (
+    <input
+      ref={ref}
+      className={cn(
+        "w-full rounded-lg border border-gray-300 px-3 py-2 text-sm focus:border-blue-500 focus:ring-1 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:focus:border-blue-400 dark:focus:ring-blue-400",
+        className
+      )}
+      {...props}
+    />
+  );
+});
 
 TextField.displayName = "TextField";
