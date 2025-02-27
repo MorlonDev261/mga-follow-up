c08f130532eb72ee1d9b305cb7f77792380aa666
MorlonDev261 <mikandrapp@gmail.com>
2025-02-28 02:59:20 +0300

    Update DataTableMain.tsx

--- src/app/_components/DataTable/DataTableMain.tsx
+++ src/app/_components/DataTable/DataTableMain.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 import { ColumnDef, ColumnFiltersState, SortingState, VisibilityState, flexRender, getCoreRowModel, getFilteredRowModel, getPaginationRowModel, getSortedRowModel, useReactTable } from "@tanstack/react-table"
-import { ArrowUpDown, ChevronDown, MoreHorizontal } from "lucide-react"
+import { ChevronDown, MoreHorizontal } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { Checkbox } from "@/components/ui/checkbox"
 import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
