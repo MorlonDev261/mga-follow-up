88b437f1af0273df4700d4f7aeab07726d7cbf67
MorlonDev261 <mikandrapp@gmail.com>
2025-02-28 21:48:24 +0300

    Update page.tsx

--- src/app/(root)/stock/page.tsx
+++ src/app/(root)/stock/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BsShopWindow } from "react-icons/bs";
+import { FaPlus } from "react-icons/fa6";
 import Balance from "@components/Balance";
 import Statistique from '@components/Statistique';
 import DataTableMain from "@components/DataTable/DataTableMain";
@@ -15,7 +16,11 @@ export default function Dashboard() {
             subtitle={<>= Total Sales : <Counter end={4350} duration={0.8} /></>}
             balance={<><Counter end={5220500} duration={0.8} sound="on" src="/sounds/money-sound.mp3" /> Ar</>}
             color="text-blue-500"
-          />
+          >
+            <button className="flex items-center justify-center gap-1 rounded bg-blue-500 px-2 py-1 text-sm text-white">
+              <FaPlus /> New arrival
+            </button>
+         </Balance>
         </div>
 
         <div className="bg-[#111] text-white p-2">
