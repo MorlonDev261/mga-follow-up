d101ffd78c24715c523d0fdf8856700b21858d57
MorlonDev261 <mikandrapp@gmail.com>
2025-02-28 23:13:13 +0300

    Update page.tsx

--- src/app/(root)/stock/page.tsx
+++ src/app/(root)/stock/page.tsx
@@ -1,42 +1,7 @@
-import { BsShopWindow } from "react-icons/bs";
-import { FaPlus } from "react-icons/fa6";
-import Balance from "@components/Balance";
-import Statistique from "@components/Statistique";
-import DataTableMain from "@components/DataTable/DataTableMain";
-import Counter from "@components/Counter"; // Assure-toi que Counter est bien importé
+import React from "react";
 
 export default function Stock() {
   return (
-    <main className="p-2">
-      <div className="grid gap-4 md:grid-cols-2">
-        {/* Balance Component */}
-        <div className="bg-[#111] p-2">
-          <Balance 
-            title={<><BsShopWindow key="icon" /> Stock Available</>} 
-            subtitle={<>Total Sales: <Counter end={4350} duration={0.8} /></>}
-            balance={<><Counter end={5220500} duration={0.8} sound="on" src="/sounds/money-sound.mp3" /> Ar</>}
-            
-          >
-            <button 
-              type="button" 
-              className="flex items-center justify-center gap-1 rounded bg-blue-500 px-2 py-1 text-sm text-white"
-            >
-              <FaPlus /> New arrival
-            </button>
-          </Balance>
-        </div>
-
-        {/* Statistique Component */}
-        <div className="bg-[#111] text-white p-2">
-          <h3 className="mb-4">Statistique de vente:</h3>
-          <div className="mb-5 w-full">
-            <Statistique type="courbe" legende={false} />
-          </div>
-        </div>
-      </div>
-
-      {/* Data Table */}
-      <DataTableMain />
-    </main>
+    <h3>Hello</h3>
   );
 }
