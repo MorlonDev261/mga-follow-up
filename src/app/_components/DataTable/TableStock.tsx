d8fa4b4c3183ada71beb03c6cf970d5f701acbe0
MorlonDev261 <mikandrapp@gmail.com>
2025-03-01 01:06:16 +0300

    Update TableStock.tsx

--- src/app/_components/DataTable/TableStock.tsx
+++ src/app/_components/DataTable/TableStock.tsx
@@ -118,6 +118,35 @@ export const columns: ColumnDef<Payment>[] = [
       );
     },
   },
+  {
+    id: "actions",
+    enableHiding: false,
+    cell: ({ row }) => {
+      const payment = row.original
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(payment.id)}
+            >
+              Copy payment ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    },
+  },
 ];
 
 export default function TableStock() {
