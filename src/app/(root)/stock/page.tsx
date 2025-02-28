feb2f2f067d0678e1101802f3fb42f7f6383e218
MorlonDev261 <mikandrapp@gmail.com>
2025-02-28 23:16:55 +0300

    Update page.tsx

--- src/app/(root)/stock/page.tsx
+++ src/app/(root)/stock/page.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { BsShopWindow } from "react-icons/bs";
+import { FaPlus } from "react-icons/fa6";
+import Balance from "@components/Balance";
 
 export default function Stock() {
   return (
-    <h3>Hello</h3>
+    <main className="p-2">
+      <div className="grid gap-4 md:grid-cols-2">
+        {/* Balance Component */}
+        <div className="bg-[#111] p-2">
+          <Balance 
+            title={<><BsShopWindow key="icon" /> Stock Available</>} 
+            subtitle={<>Total Sales: <Counter end={4350} duration={0.8} /></>}
+            balance={<><Counter end={5220500} duration={0.8} sound="on" src="/sounds/money-sound.mp3" /> Ar</>}
+            color="text-blue-500"
+          >
+            <button 
+              type="button" 
+              className="flex items-center justify-center gap-1 rounded bg-blue-500 px-2 py-1 text-sm text-white"
+            >
+              <FaPlus /> New arrival
+            </button>
+          </Balance>
+        </div>
+      </div>
+    </main>
   );
 }
