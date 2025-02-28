b017eedf6d56bd39bf68761ad34c6df5a2f98ed4
MorlonDev261 <mikandrapp@gmail.com>
2025-02-28 21:38:56 +0300

    Update page.tsx

--- src/app/(root)/stock/page.tsx
+++ src/app/(root)/stock/page.tsx
@@ -3,10 +3,9 @@ import { BsShopWindow } from "react-icons/bs";
 import Balance from "@components/Balance";
 import Statistique from '@components/Statistique';
 import DataTableMain from "@components/DataTable/DataTableMain";
-
+import Counter from "@components/Counter"; // Assure-toi que Counter est bien importé
 
 export default function Dashboard() {
-  
   return (
     <main className="p-2">
       <div className="grid gap-4 md:grid-cols-2">
@@ -15,8 +14,10 @@ export default function Dashboard() {
             title={<><BsShopWindow /> Stock Available</>} 
             subtitle={<>= Total Sales : <Counter end={4350} duration={0.8} /></>}
             balance={<><Counter end={5220500} duration={0.8} sound="on" src="/sounds/money-sound.mp3" /> Ar</>}
-          >
+            color="text-blue-500"
+          />
         </div>
+
         <div className="bg-[#111] text-white p-2">
           <h3 className="mb-4">Statistique de vente:</h3>
           <div className="mb-5 w-full">
@@ -24,8 +25,8 @@ export default function Dashboard() {
           </div>
         </div>
       </div>
-    </div>
-    <DataTableMain />
-   </main>
- );
+
+      <DataTableMain />
+    </main>
+  );
 }
