e3668b9678383dcb4f3d361b62940f5d7d858344
MorlonDev261 <mikandrapp@gmail.com>
2025-02-28 18:35:12 +0300

    Create page.tsx

---
+++ src/app/(second)/excel/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import ExcelTab from "@/components/ExcelTab";
+
+export default function HomeExcel() {
+  return (
+    <main className="p-1">
+      <div className="bg-[#111]">
+        <h3 className="text-xl px-1 mb-4">Financial Tracking</h3>
+        <ExcelTab />
+      </div>
+    </main>
+  );
+}
