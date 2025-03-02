f1b44efb52f7b402c4cefcd4e4e6fca03f630e8d
MorlonDev261 <mikandrapp@gmail.com>
2025-03-02 18:49:29 +0300

    Update searchUtils.ts

--- src/lib/searchUtils.ts
+++ src/lib/searchUtils.ts
@@ -1,6 +1,18 @@
 import { parse, isValid, format } from 'date-fns';
 import { fr } from 'date-fns/locale';
-import type { Product } from "@/app/_components/DataTable/TableStock";
+
+type Product = {
+  id: string;
+  date: string;
+  Qte?: number;
+  designation: string;
+  idProduct: string;
+  comments: string;
+  amount: number;
+  total?: number;
+  dateObject?: Date;
+  _search?: string;
+};
 
 type SearchConditions = {
   dateFilters: Date[];
