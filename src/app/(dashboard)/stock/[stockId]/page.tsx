d6d2d1a5b6ece566d2070523a0e5e03f36d8a8f1
MorlonDev261 <mikandrapp@gmail.com>
2025-03-02 21:55:28 +0300

    Update page.tsx

--- src/app/(dashboard)/stock/[stockId]/page.tsx
+++ src/app/(dashboard)/stock/[stockId]/page.tsx
@@ -6,8 +6,8 @@ export default function MyComponent() {
   return (
     <>
       <div className="p-2 bg-[#111]">
-        <h3>Arrival 20 March 2025</h3>
-        <h4 className="text-blue-500">Total : <span className="text-bold">25pcs</span></h4>
+        <h2>Arrival 20 March 2025</h2>
+        <h3 className="text-blue-500">Total : <span className="text-bold">25pcs</span></h3>
         {inStock ? 
           <p className="text-yello-500">Remaining : <span className="text-bold">5pcs</span></p> : 
           <p className="text-green-500">Out of Stock</p>
