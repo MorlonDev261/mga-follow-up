73985420c042767f414cd46e8f46404893be5c42
MorlonDev261 <mikandrapp@gmail.com>
2025-03-02 19:40:21 +0300

    Update searchUtils.ts

--- src/lib/searchUtils.ts
+++ src/lib/searchUtils.ts
@@ -98,12 +98,11 @@ export function filterData<T extends { _search: string; dateObject: Date }>(
     // Vérification des dates
     const dateMatch = dateFilters.length === 0 || dateFilters.some(filterDate => {
       const itemDate = item.dateObject;
-      if (!itemDate) return false; // Évite les erreurs si `dateObject` est `undefined`
-        return (
-          format(filterDate, 'yyyy-MM-dd') === format(itemDate, 'yyyy-MM-dd') ||
-          format(filterDate, 'yyyy-MM') === format(itemDate, 'yyyy-MM') ||
-          format(filterDate, 'yyyy') === format(itemDate, 'yyyy')
-        );
+      return (
+        format(filterDate, 'yyyy-MM-dd') === format(itemDate, 'yyyy-MM-dd') ||
+        format(filterDate, 'yyyy-MM') === format(itemDate, 'yyyy-MM') ||
+        format(filterDate, 'yyyy') === format(itemDate, 'yyyy')
+      );
     });
 
     // Vérification du texte
