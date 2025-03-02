bc2a921e14f6c4003f2163e15437bec2c78028a0
MorlonDev261 <mikandrapp@gmail.com>
2025-03-02 21:44:56 +0300

    Update page.tsx

--- src/app/(dashboard)/stock/[stockId]/page.tsx
+++ src/app/(dashboard)/stock/[stockId]/page.tsx
@@ -2,6 +2,16 @@ import TableStock from "@components/DataTable/TableStock";
 
 export default function MyComponent() {
   return (
+    <>
+      <h3>Arrival 20 March 2025</h3>
+      <h4 className="text-blue-500">Total : <span className="text-bold">25pcs</span></h4>
+      <div>
+        {inStock ? 
+          <p className="text-yello-500">Remaining : <span className="text-bold">5pcs</span></p> : 
+          <p className="text-green-500">Out of Stock</p>
+        }
+      </div>
       <TableStock />
+    </>
   );
 }
