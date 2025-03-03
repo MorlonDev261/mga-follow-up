775e2fd4e7d7251cc60e1492b746d59c98d4fb91
MorlonDev261 <mikandrapp@gmail.com>
2025-03-03 19:06:41 +0300

    Update page.tsx

--- src/app/(dashboard)/customers/page.tsx
+++ src/app/(dashboard)/customers/page.tsx
@@ -30,7 +30,7 @@ export default function MyComponent() {
           </Suspense> 
         </div>
         :
-        <div className="w-full h-[60vh] mt-5 flex items-center justify-center flex-col bg-[#111]">
+        <div className="w-full h-[65vh] mt-5 flex items-center justify-center flex-col bg-[#111]">
           <p>There is no customer. please add your customer !</p>
           <button className="px-4 flex items-center gap-2 py-2 bg-green-500 hover:bg-green-600"><FaPlus /> Add First Customer</button>
         </div>
