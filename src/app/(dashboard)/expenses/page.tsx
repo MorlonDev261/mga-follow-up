6a6c4bfe2178487b7e038dd674d6ddc722e2cde6
MorlonDev261 <mikandrapp@gmail.com>
2025-03-03 22:52:55 +0300

    Update page.tsx

--- src/app/(dashboard)/expenses/page.tsx
+++ src/app/(dashboard)/expenses/page.tsx
@@ -13,10 +13,13 @@ export default function MyComponent() {
         <Balance 
           title={<><FaUsers /> List of Customers</>} 
           balance={expenses > 0 ? `Total expenses : ${expenses} now.` : "No expense added."}
+          balanceSize="text-md"
+          balanceColor="text-red-500 hover:text-red-600"
           subtitle="The expense resets to 0 when the cash register is empty."
+          subtitleSize="text-sm"
         >
           {expenses > 0 &&
-            <button className="flex items-center justify-center gap-1 rounded bg-yellow-500 hover:bg-yellow-600 px-2 py-1 text-sm text-white">
+            <button className="flex items-center gap-1 rounded bg-red-500 hover:bg-red-600 px-2 py-1 text-sm text-white">
               <FaPlus /> New expense
             </button>
           }
