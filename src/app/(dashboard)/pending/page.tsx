22fc8089f48772364dfbd212d1f33ea63028c660
MorlonDev261 <mikandrapp@gmail.com>
2025-03-03 13:13:02 +0300

    Update page.tsx

--- src/app/(dashboard)/pending/page.tsx
+++ src/app/(dashboard)/pending/page.tsx
@@ -2,16 +2,15 @@ import React, { Suspense } from "react";
 import TableStock from "@components/DataTable/TableStock";
 
 export default function MyComponent() {
-  const inStock = false;
+  const pending = true;
   
   return (
     <>
       <div className="px-2 bg-[#111]">
         <h2 className="text-lg">Pending payment</h2>
-        <h3 className="text-blue-500">Total : <span className="text-bold">25pcs</span></h3>
-        {inStock ? 
-          <p className="text-yello-500">Remaining : <span className="text-bold">5pcs</span></p> : 
-          <p className="text-green-500">Out of Stock</p>
+        {pending ? 
+          <h3 className="text-blue-500">250,000 Ar</h3> : 
+          <p className="text-green-500">No pending payement.</p>
         }
       </div>
       <Suspense fallback={<div>Loading...</div>}>
