e9c7bbcc81e8b06dda3e991851b12c6733d66d61
MorlonDev261 <mikandrapp@gmail.com>
2025-03-03 21:12:59 +0300

    Update page.tsx

--- src/app/(dashboard)/pending/page.tsx
+++ src/app/(dashboard)/pending/page.tsx
@@ -5,7 +5,7 @@ export default function MyComponent() {
   const pending = true;
   
   return (
-    <>
+    <main>
       <div className="px-2 bg-[#111]">
         <h2 className="text-lg">Pending payment</h2>
         {pending ? 
@@ -13,9 +13,9 @@ export default function MyComponent() {
           <p className="text-green-500">No pending payement.</p>
         }
       </div>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense>
         <TableStock />
       </Suspense>
-    </>
+    <main/>
   );
 }
