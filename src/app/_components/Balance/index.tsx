0e875b5c187969b3945323fb1c777b2df3ca482a
MorlonDev261 <mikandrapp@gmail.com>
2025-03-03 14:46:40 +0300

    Update index.tsx

--- src/app/_components/Balance/index.tsx
+++ src/app/_components/Balance/index.tsx
@@ -1,28 +1,34 @@
 "use client";
 import { ReactNode } from "react";
+import { cn } from "@/lib/utils";
 
 interface BalanceProps {
   title: ReactNode;
+  titleColor?: string;
+  titleSize?: string;
   subtitle: ReactNode;
+  subtitleColor?: string;
+  subtitleSize?: string;
   balance: ReactNode;
-  textColor?: string;
+  balanceColor?: string;
+  balanceSize?: string
   children: ReactNode;
 }
 
-export default function Balance({ title, balance, textColor = "text-green-500", subtitle, children }: BalanceProps) {
+export default function Balance({ title, titleColor, titleSize, balance, balanceColor, balanceSize, subtitle, subtitleColor, subtitleSize, children }: BalanceProps) {
   
   return (
     <div className="mb-4">
-      <h3 className="text-gray-400 flex items-center gap-1">
+      <h3 className={cn("flex items-center gap-1", titleColor ? titleColor: "text-gray-400", titleSize)}>
         {title}
       </h3>
-      <div className="my-2 flex items-center justify-between text-2xl font-bold">
-        <div className={textColor}>
+      <div className="my-2 flex items-center justify-between font-bold">
+        <div className={cn(balanceColor ? balanceColor : "text-green-500", balanceSize ? balanceSize : "text-2xl")}>
           {balance}
         </div>
         {children}
       </div>
-      <p className="text-gray-400">
+      <p className={cn(subtitleColor ? subtitleColor : "text-gray-400", subtitleSize)}>
         {subtitle}
       </p>
     </div>
