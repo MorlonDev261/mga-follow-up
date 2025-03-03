760482b8032d8f4001164e8aa45855d07a266e08
MorlonDev261 <mikandrapp@gmail.com>
2025-03-03 18:22:17 +0300

    Update page.tsx

--- src/app/(dashboard)/customers/page.tsx
+++ src/app/(dashboard)/customers/page.tsx
@@ -8,7 +8,7 @@ export default function MyComponent() {
   
   return (
     <div className="h-screen">
-      <div className="px-2 bg-[#111]">
+      <div className="p-2 bg-[#111]">
         <Balance 
           title={<><FaUsers /> List of Customers</>} 
           subtitle={customers > 0 ? "26 customers now." : "No customer registered."}
@@ -23,10 +23,41 @@ export default function MyComponent() {
         </Balance>
       </div>
       {customers > 0 ?
-        <Suspense fallback={<div>Loading...</div>}>
+        <div className="mt-2 bg-[#111] w-full h-[100%]">
+          <Suspense fallback={<div>Loading...</div>}>
           <TableStock />
-        </Suspense> :
-        <div className="w-full h-full mt-5 flex items-center justify-center flex-col bg-[#111]">
+        import React, { Suspense } from "react";
+import TableStock from "@components/DataTable/TableStock";
+import Balance from "@components/Balance";
+import { FaPlus, FaUsers } from "react-icons/fa";
+
+export default function MyComponent() {
+  const customers = 0;
+  
+  return (
+    <>
+      <div className={cn("px-2", customers <= 0 && "bg-[#111]")}>
+        <Balance 
+          title={<><FaUsers /> List of Customers</>} 
+          subtitle={customers > 0 ? "26 customers now." : "No customer registered."}
+          subtitleSize="text-xl"
+          subtitleColor="text-green-500"
+        >
+          {customers > 0 &&
+            <button className="flex items-center justify-center gap-1 rounded bg-blue-500 hover:bg-blue-600 px-2 py-1 text-sm text-white">
+              <FaPlus /> New customer
+            </button>
+          }
+        </Balance>
+      </div>
+      {customers > 0 ?
+        <div className="w-full h-[100%] mt-2 flex items-center justify-center flex-col bg-[#111]">
+          <Suspense fallback={<div>Loading...</div>}>
+            <TableStock />
+          </Suspense> 
+        </div>
+        :
+        <div className="w-full h-[60vh] mt-5 flex items-center justify-center flex-col bg-[#111]">
           <p>There is no customer. please add your customer !</p>
           <button className="px-4 flex items-center gap-2 py-2 bg-green-500 hover:bg-green-600"><FaPlus /> Add First Customer</button>
         </div>
