facbc2dcdce7ba348260ab57ec7fab285996c074
MorlonDev261 <mikandrapp@gmail.com>
2025-03-04 10:36:29 +0300

    Update page.tsx

--- src/app/(dashboard)/pending/page.tsx
+++ src/app/(dashboard)/pending/page.tsx
@@ -1,21 +1,45 @@
 import React, { Suspense } from "react";
-import Pending from "@components/Table/Pending";
+import Customers from "@components/Table/Customers";
+import Balance from "@components/Balance";
+import { FaPlus } from "react-icons/fa";
+import { FiClock } from 'react-icons/fi';
+import { cn } from "@/lib/utils";
 
 export default function MyComponent() {
-  const pending = true;
+  const expenses = 12;
   
   return (
-    <main>
+    <main className={cn(expenses <= 0 && "bg-[#111]")}>
       <div className="px-2 bg-[#111]">
-        <h2 className="text-lg">Pending payment</h2>
-        {pending ? 
-          <h3 className="text-blue-500">250,000 Ar</h3> : 
-          <p className="text-green-500">No pending payement.</p>
-        }
+        <Balance 
+          title={<><FiClock /> Pending Payement</>} 
+          balance={expenses > 0 ? `${totalPending} Ar.` : "No pending payement added."}
+          balanceSize="text-md"
+          balanceColor="text-yellow-500 hover:text-yellow-600"
+          subtitle="25 customers have pending payments."
+          subtitleSize="text-sm"
+        >
+          {expenses > 0 &&
+            <button className="flex items-center gap-1 rounded bg-red-500 hover:bg-red-600 px-2 py-1 text-sm text-white">
+              <FaPlus /> New expense
+            </button>
+          }
+        </Balance>
       </div>
-      <Suspense>
-        <Pending />
-      </Suspense>
+      {expenses > 0 ?
+        <div className="pt-2 bg-[#111]">
+          <Suspense>
+            <Customers />
+          </Suspense> 
+        </div>
+        :
+        <div className="w-full h-[65vh] flex items-center justify-center bg-[#111]">
+          <div className="-mt-5 flex items-center justify-center flex-col ">
+            <p>There is nothing to see. Please record a first sale that has not been paid yet.</p>
+            <button className="px-4 mt-2 flex items-center gap-2 py-2 bg-yellow-500 hover:bg-yellow-600"><FaPlus /> Add First Pending Payement</button>
+          </div>
+        </div>
+      }
     </main>
   );
 }
