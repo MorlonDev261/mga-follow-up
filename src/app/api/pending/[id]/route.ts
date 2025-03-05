c04152e7b5fe301e3634994fd490fe8e9b4118e4
MorlonDev261 <mikandrapp@gmail.com>
2025-03-05 18:02:19 +0300

    Update route.ts

--- src/app/api/pending/[id]/route.ts
+++ src/app/api/pending/[id]/route.ts
@@ -1,8 +1,19 @@
 import { NextResponse } from "next/server";
 
+type Payment = {
+  id: string
+  date: string
+  customer: string
+  designation: string
+  price: number
+}
+
 const payments: Payment[] = [
-  { id: "1", date: "2023-10-01", customer: "John Doe", designation: "Website Design", price: 500 },
-  { id: "2", date: "2023-10-02", customer: "Jane Smith", designation: "SEO Optimization", price: 300 },
+  { id: "m5gr84i9", date: "23-01-25", customer: "Kiady", designation: "iPhone 8", price: 316 },
+  { id: "3u1reuv4", date: "01-02-25", customer: "Mr Tanjona", designation: "iPhone 11pro 64", price: 316 },
+  { id: "derv1ws0", date: "12-01-25", customer: "Top Mada", designation: "Moto G 128", price: 316 },
+  { id: "5kma53ae", date: "04-02-25", customer: "Kiady", designation: "iPhone Xs", price: 316 },
+  { id: "bhqecj4p", date: "23-02-25", customer: "Shop Cell", designation: "iPhone 7plus", price: 10034 },
 ];
 
 export async function GET(request: Request, { params }: { params: { id: string } }) {
