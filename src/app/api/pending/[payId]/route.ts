c76671f4985216b8c30a7438244e5f34517961a9
MorlonDev261 <mikandrapp@gmail.com>
2025-03-06 13:35:01 +0300

    Update route.ts

--- src/app/api/pending/[payId]/route.ts
+++ src/app/api/pending/[payId]/route.ts
@@ -9,15 +9,18 @@ type Payment = {
 };
 
 const payments: Payment[] = [
-  { id: "m5gr84i9", date: "23-01-25", customer: "Kiady", designation: "iPhone 8", price: 316 },
-  { id: "3u1reuv4", date: "01-02-25", customer: "Mr Tanjona", designation: "iPhone 11pro 64", price: 316 },
-  { id: "derv1ws0", date: "12-01-25", customer: "Top Mada", designation: "Moto G 128", price: 316 },
-  { id: "5kma53ae", date: "04-02-25", customer: "Kiady", designation: "iPhone Xs", price: 316 },
-  { id: "bhqecj4p", date: "23-02-25", customer: "Shop Cell", designation: "iPhone 7plus", price: 10034 },
+  { id: "m5gr84i9", date: "2025-01-23", customer: "Kiady", designation: "iPhone 8", price: 316 },
+  { id: "3u1reuv4", date: "2025-02-01", customer: "Mr Tanjona", designation: "iPhone 11pro 64", price: 316 },
+  { id: "derv1ws0", date: "2025-01-12", customer: "Top Mada", designation: "Moto G 128", price: 316 },
+  { id: "5kma53ae", date: "2025-02-04", customer: "Kiady", designation: "iPhone Xs", price: 316 },
+  { id: "bhqecj4p", date: "2025-02-23", customer: "Shop Cell", designation: "iPhone 7plus", price: 10034 },
 ];
 
-export async function GET(req: NextRequest, context: { params: any }) {
-  const { payId } = context.params;
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { payId: string } } // ✅ Correction ici
+) {
+  const { payId } = params;
 
   const payment = payments.find((p) => p.id === payId);
 
