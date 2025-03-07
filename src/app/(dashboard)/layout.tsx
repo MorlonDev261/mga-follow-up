62d23f7f1c5ca495379a0e41bcc9d9f57723ead0
MorlonDev261 <mikandrapp@gmail.com>
2025-03-08 02:19:25 +0300

    Update layout.tsx

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -4,12 +4,12 @@ import { useState } from "react";
 import Header from "@components/Header";
 import Sidebar from "@components/Sidebar";
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
-  const [open, setOpen] = useState(false);
+  const [sidebarStatus, setSidebarStatus] = useState(false);
   return (
     <div className="flex">
-      <Header open={open} setOpen={setOpen} />
+      <Header open={sidebarStatus} setOpen={setSidebarStatus} />
       <aside>
-        <Sidebar open={open} setOpen={setOpen} />
+        <Sidebar open={sidebarStatus} setOpen={setSidebarStatus} />
       </aside>
       <main>
         {children}
