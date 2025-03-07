81f3b48c60d5050d92447382b6e2374991b830dd
MorlonDev261 <mikandrapp@gmail.com>
2025-03-07 19:53:20 +0300

    Update index.tsx

--- src/app/_components/StockList/index.tsx
+++ src/app/_components/StockList/index.tsx
@@ -1,14 +1,14 @@
 import { FaRegCalendarAlt } from "react-icons/fa";
 
 const stocks = [
-  { label: 'Stock 24-03-25', inStock: 457900, sales: 3636 },
-  { label: 'Stock 01-02-25', inStock: 457900, sales: 846 },
-  { label: 'Pending', inStock: 457900, sales: 5353 },
-  { label: 'Employers', inStock: 7, sales: 35 },
-  { label: 'Payement', inStock: 53, sales: 386 },
-  { label: 'Customers', inStock: 7, sales: 5263 },
-  { label: 'Profile', inStock: 568, sales: 56 },
-  { label: 'Settings', inStock: 567, sales: 56 },
+  { label: '24-03-25', inStock: 457900, sales: 3636 },
+  { label: '01-02-25', inStock: 457900, sales: 846 },
+  { label: '24-01-25', inStock: 457900, sales: 5353 },
+  { label: '07-02-25', inStock: 7, sales: 35 },
+  { label: '10-02-25', inStock: 53, sales: 386 },
+  { label: '14-02-25', inStock: 7, sales: 5263 },
+  { label: '06-03-25', inStock: 568, sales: 56 },
+  { label: '08-03-25', inStock: 567, sales: 56 },
 ];
 
 const StockList = () => {
