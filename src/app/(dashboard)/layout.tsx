b67bb5aa313c1f45f1a612468d4b9668e8363dc1
MorlonDev261 <mikandrapp@gmail.com>
2025-03-08 22:51:25 +0300

    Update layout.tsx

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,9 +1,6 @@
 import Header from "@components/Header";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-      {/*<aside role="navigation">
-        <Sidebar open={open} setOpen={setOpen} />
-      </aside>*/}
         <Header />
         {children}
   );
