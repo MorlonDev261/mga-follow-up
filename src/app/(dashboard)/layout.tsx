cf8af7ffcc15cac798e4d2b11b311b7e9eb35f73
MorlonDev261 <mikandrapp@gmail.com>
2025-03-08 12:24:26 +0300

    Update layout.tsx

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,19 +1,12 @@
-"use client";
-
-import { useState } from "react";
 import Header from "@components/Header";
-import Sidebar from "@components/Sidebar";
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const [open, setOpen] = useState(false);
-
   return (
     <div className="flex h-screen">
-      <aside role="navigation">
+      {/*<aside role="navigation">
         <Sidebar open={open} setOpen={setOpen} />
-      </aside>
+      </aside>*/}
       <div className="flex-1 flex flex-col">
-        <Header open={open} setOpen={setOpen} />
+        <Header />
         <main className="flex-1 overflow-auto">{children}</main>
       </div>
     </div>
