6ad752507cb7ffc069507d9b3673dbbebecd08de
MorlonDev261 <mikandrapp@gmail.com>
2025-03-08 20:14:23 +0300

    Update index.tsx

--- src/app/_components/Theme/index.tsx
+++ src/app/_components/Theme/index.tsx
@@ -19,7 +19,7 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <span className="text-xl">
+        <span className="text-xl hover">
           <MdWbSunny className="text-xl rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <BsMoonStarsFill className="absolute text-xl rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
