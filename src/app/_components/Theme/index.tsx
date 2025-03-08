eeaf7a7f1cf998130b91bb9c3955317b40e42a4b
MorlonDev261 <mikandrapp@gmail.com>
2025-03-08 20:13:59 +0300

    Create index.tsx

---
+++ src/app/_components/Theme/index.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import * as React from "react"
+import { BsMoonStarsFill } from "react-icons/bs"
+import { MdWbSunny } from "react-icons/md";
+import { useTheme } from "next-themes"
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+export function ModeToggle() {
+  const { setTheme } = useTheme()
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <span className="text-xl">
+          <MdWbSunny className="text-xl rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <BsMoonStarsFill className="absolute text-xl rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </span>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="center">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
