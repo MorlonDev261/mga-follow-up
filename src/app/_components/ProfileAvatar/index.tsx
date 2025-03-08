25307498c47abf36aa46e0f7c9f22abd0132fe9b
MorlonDev261 <mikandrapp@gmail.com>
2025-03-08 16:58:36 +0300

    Update index.tsx

--- src/app/_components/ProfileAvatar/index.tsx
+++ src/app/_components/ProfileAvatar/index.tsx
@@ -34,8 +34,8 @@ export default function ProfileAvatar({ auth=false }: ProfileProps) {
         <AvatarImage 
             src={src}
             alt="Profile"
-            width={80}
-            height={80}
+            width={100}
+            height={100}
             className="rounded-full object-cover"
             onError={() => setError(true)}
         />
