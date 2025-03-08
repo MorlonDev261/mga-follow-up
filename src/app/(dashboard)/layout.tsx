3f298a1740d02a64f105acc9db404afe33fbb8a9
MorlonDev261 <mikandrapp@gmail.com>
2025-03-08 22:41:45 +0300

    Update layout.tsx

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,14 +1,10 @@
 import Header from "@components/Header";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen">
       {/*<aside role="navigation">
         <Sidebar open={open} setOpen={setOpen} />
       </aside>*/}
-      <div className="flex-1 flex flex-col">
         <Header />
-        <main className="flex-1 overflow-y-auto">{children}</main>
-      </div>
-    </div>
+        {children}
   );
 }
