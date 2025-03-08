d4550fb174b2f024451e894bfa433ed7af2607dd
MorlonDev261 <mikandrapp@gmail.com>
2025-03-08 22:59:33 +0300

    Update layout.tsx

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,7 +1,10 @@
 import Header from "@components/Header";
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-        <Header />
-        {children}
+    <>
+      <Header />
+      {children}
+    </>
   );
 }
