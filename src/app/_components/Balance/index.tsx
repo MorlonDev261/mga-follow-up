d8bade62fd91f8e6dc90740293a4440542dc7e97
MorlonDev261 <mikandrapp@gmail.com>
2025-03-10 12:18:28 +0300

    Update index.tsx

--- src/app/_components/Balance/index.tsx
+++ src/app/_components/Balance/index.tsx
@@ -18,7 +18,7 @@ interface BalanceProps {
 export default function Balance({ title, titleColor, titleSize, balance, balanceColor, balanceSize, subtitle, subtitleColor, subtitleSize, children }: BalanceProps) {
   
   return (
-    <div className="m-1 shadow-md sm:shadow-none">
+    <div className="my-2 p-2 shadow-md sm:shadow-none">
       <h3 className={cn("flex items-center gap-1", titleColor ? titleColor: "text-gray-400", titleSize)}>
         {title}
       </h3>
