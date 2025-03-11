031df4adefa1eb285dd23fa4fefc3daabe2e805f
MorlonDev261 <mikandrapp@gmail.com>
2025-03-11 16:35:03 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,6 +57,7 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="manifest" href="/manifest.json" />
 
         {/* Script pour éviter le flash blanc */}
         <script dangerouslySetInnerHTML={{
