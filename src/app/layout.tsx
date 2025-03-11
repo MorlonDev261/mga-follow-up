03630ffbc2caf08c46b6c15e3c95b0da3f4093ce
MorlonDev261 <mikandrapp@gmail.com>
2025-03-12 01:27:28 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,6 @@ export function generateMetadata(): Metadata {
       images: [
         {
           url: "https://yourwebsite.com/og-image.jpg",
-          secure_url: "https://yourwebsite.com/og-image.jpg",
           width: 1200,
           height: 630,
           alt: "MGA Follow UP Dashboard",
