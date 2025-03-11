06f06cef299de599c5d5d2830e2c895befb46cd2
MorlonDev261 <mikandrapp@gmail.com>
2025-03-11 14:25:39 +0300

    Update index.tsx

--- src/app/_icons/index.tsx
+++ src/app/_icons/index.tsx
@@ -5,7 +5,7 @@ export function SwapWallet() {
   return (
     <div className="relative">
       <IoWalletOutline size={24} />
-      <CgArrowsExchangeAlt className="absolute rounded-full bg-white dark:bg-gray-500 text-sm bottom-0 w-1 h-1 -right-2" />
+      <CgArrowsExchangeAlt className="absolute rounded-full bg-white dark:bg-gray-500 text-sm bottom-0 w-5 h-5 -right-2" />
     </div>
   );
 }
