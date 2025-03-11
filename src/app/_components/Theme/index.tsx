d6c9a79356fb7d00a324973d303d5dad5df9a854
MorlonDev261 <mikandrapp@gmail.com>
2025-03-11 11:19:13 +0300

    Update index.tsx

--- src/app/_components/Theme/index.tsx
+++ src/app/_components/Theme/index.tsx
@@ -41,7 +41,7 @@ export default function ToggleTheme() {
               <MdWbSunny className="text-xl text-yellow-400" />
             )
           ) : (
-            <div className="w-xl text-gray-800 dark:text-gray-200" />
+            <MdWbSunny className="text-xl hidden text-gray-800 dark:text-gray-200" />
           )}
           <span className="sr-only">Toggle theme</span>
         </span>
