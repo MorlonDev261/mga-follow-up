a487cfce41443fc2b9fa439c7854500c6fce9523
MorlonDev261 <mikandrapp@gmail.com>
2025-03-12 14:26:56 +0300

    Create db.ts

---
+++ src/lib/db.ts
@@ -0,0 +1,41 @@
+import { Low, JSONFile } from "lowdb";
+
+export interface User {
+  id: string;
+  name: string;
+  avatar?: string; // URL de l'image de profil
+  isOnline?: boolean; // Statut en ligne
+}
+
+export interface Chat {
+  id: string;
+  name: string;
+  avatar: string;
+  lastMessage: string;
+  lastMessageTimestamp: number;
+  unread: number;
+}
+
+export interface Message {
+  id: string;
+  text: string;
+  timestamp: string;
+  isOwn: boolean;
+  senderId: string;
+  chatId: string;
+  read: boolean;
+  avatar: string;
+}
+
+interface Data {
+  users: User[];
+  chats: Chat[];
+  messages: Message[];
+}
+
+// Fichier JSON où seront stockées les données
+const file = "db.json";
+const adapter = new JSONFile<Data>(file);
+const db = new Low<Data>(adapter, { users: [], chats: [], messages: [] });
+
+export default db;
