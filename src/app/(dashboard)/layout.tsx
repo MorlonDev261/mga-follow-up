80e9212fa3629c14a87214f9b267597545d1a9b8
MorlonDev261 <mikandrapp@gmail.com>
2025-03-12 03:25:31 +0300

    Update layout.tsx

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,4 @@
 import Header from "@components/Header";
-//import Download from "@components/Download";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
