b6d8378a11a69204729e385fb48bc64a4934148b
MorlonDev261 <mikandrapp@gmail.com>
2025-03-12 22:03:27 +0300

    Update ChatHeader.tsx

--- src/app/messages/_components/ChatHeader.tsx
+++ src/app/messages/_components/ChatHeader.tsx
@@ -13,31 +13,32 @@ const ChatHeader = ({
   const isOnline = chat ? true : false; // Remplace ceci par un vrai état "online"
 
   return (
-    <div className="border-b p-2 flex items-center gap-3">
+    <div className="border-b p-2 flex items-center justify-between">
       {/* Bouton Retour sur mobile */}
-      {onBack && (
-        <button onClick={onBack} className="mr-2 p-2 rounded-full hover:bg-gray-200 dark:hover:bg-gray-700">
-          <MdOutlineArrowBackIosNew className="w-5 h-5" />
-        </button>
-      )}
+      <div className="flex items-center gap-1">
+        {onBack && (
+          <button onClick={onBack} className="mr-2 p-2 rounded-full hover:bg-gray-200 dark:hover:bg-gray-700">
+            <MdOutlineArrowBackIosNew className="w-5 h-5" />
+          </button>
+        )}
 
-      <Avatar>
-        <AvatarImage
-          src={chat?.avatar || ""}
-          alt={chat?.name || "User"}
-        />
-      </Avatar>
+        <Avatar>
+          <AvatarImage
+            src={chat?.avatar || ""}
+            alt={chat?.name || "User"}
+          />
+        </Avatar>
 
-      <div>
-        <h2 className="font-semibold">{chat?.name || "Sélectionnez une discussion"}</h2>
-        <p className="text-sm text-gray-500 dark:text-gray-400">
-          {isOnline ? "En ligne" : "Commencez une conversation"}
-        </p>
+        <div>
+          <h2 className="font-semibold">{chat?.name || "Sélectionnez une discussion"}</h2>
+          <p className="text-sm text-gray-500 dark:text-gray-400">
+            {isOnline ? "En ligne" : "Commencez une conversation"}
+          </p>
+        </div>
       </div>
-
       <div className="flex items-center gap-2">
-        <FaPhoneAlt />
-        <FaVideo />
+        <FaPhoneAlt className="bg-gradient-to-r from-green-500 to-orange-500 bg-clip-text text-transparent" />
+        <FaVideo className="bg-gradient-to-r from-green-500 to-orange-500 bg-clip-text text-transparent" />
       </div>
     </div>
   );
