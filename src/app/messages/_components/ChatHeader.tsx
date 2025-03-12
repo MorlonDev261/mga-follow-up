230cbe22a0e243d521113a8738203f424e08d55b
MorlonDev261 <mikandrapp@gmail.com>
2025-03-12 21:51:01 +0300

    Update ChatHeader.tsx

--- src/app/messages/_components/ChatHeader.tsx
+++ src/app/messages/_components/ChatHeader.tsx
@@ -1,5 +1,7 @@
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
-import { ArrowLeft } from "lucide-react";
+import { MdOutlineArrowBackIosNew } from "react-icons/md";
+import { FaPhoneAlt, FaVideo } from "react-icons/fa";
+
 
 const ChatHeader = ({
   chat,
@@ -11,11 +13,11 @@ const ChatHeader = ({
   const isOnline = chat ? true : false; // Remplace ceci par un vrai état "online"
 
   return (
-    <div className="border-b p-4 flex items-center gap-3">
+    <div className="border-b p-2 flex items-center gap-3">
       {/* Bouton Retour sur mobile */}
       {onBack && (
         <button onClick={onBack} className="mr-2 p-2 rounded-full hover:bg-gray-200 dark:hover:bg-gray-700">
-          <ArrowLeft className="w-5 h-5" />
+          <MdOutlineArrowBackIosNew className="w-5 h-5" />
         </button>
       )}
 
@@ -31,6 +33,11 @@ const ChatHeader = ({
         <p className="text-sm text-gray-500 dark:text-gray-400">
           {isOnline ? "En ligne" : "Commencez une conversation"}
         </p>
+      </div>
+
+      <div className="flex items-center gap-2">
+        <FaPhoneAlt />
+        <FaVideo />
       </div>
     </div>
   );
