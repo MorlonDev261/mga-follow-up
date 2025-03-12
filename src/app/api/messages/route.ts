1797782d11d7660946276b4dd4a87e5e2c905449
MorlonDev261 <mikandrapp@gmail.com>
2025-03-12 16:37:54 +0300

    Update route.ts

--- src/app/api/messages/route.ts
+++ src/app/api/messages/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { db } from "@/lib/db";
+import db from "@/lib/db";
 import { v4 as uuidv4 } from "uuid";
 import { Message } from "@/models/message";
 
