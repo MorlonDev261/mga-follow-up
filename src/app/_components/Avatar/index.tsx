ebd72579895c7f72ad7766d3779f61ce3e83494c
MorlonDev261 <mikandrapp@gmail.com>
2025-03-13 18:28:04 +0300

    Update index.tsx

--- src/app/_components/Avatar/index.tsx
+++ src/app/_components/Avatar/index.tsx
@@ -1,8 +1,11 @@
 import { ReactNode } from "react";
 import { cn } from "@/lib/utils";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { FaUser } from "react-ivons/fa";
 
 interface ChatAvatarProps {
+  src?: string;
+  Fallback?: ReactNode;
   isOnline?: boolean;
   isStory?: boolean;
   className?: string;
@@ -11,11 +14,12 @@ interface ChatAvatarProps {
 
 }
 
-const ProfileAvatar = ({ isOnline = false, isStory = false, className, children, badge }: ChatAvatarProps) => {
+const ProfileAvatar = ({ src, Fallback= <FaUser />, isOnline = false, isStory = false, className, children, badge }: ChatAvatarProps) => {
   return (
     <span className={cn("relative", className)}>
       <Avatar className={cn(isStory && "border-2 border-green-700")}>
-        {children}
+        <AvatarImage src={src} alt="Profile" />
+        <AvatarFallback>{Fallback}</AvatarFallback>
       </Avatar>
       {isOnline && (
         <span className="absolute bottom-0 right-0 w-3 h-3 bg-green-500 border-2 border-white rounded-full"></span>
