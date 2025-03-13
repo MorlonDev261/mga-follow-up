4c38cd5811250a957deed8fcd32eefe49b1bfdb8
MorlonDev261 <mikandrapp@gmail.com>
2025-03-14 01:19:16 +0300

    Update progress.tsx

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -1,17 +1,18 @@
+"use client";
+
+import * as React from "react";
+import * as ProgressPrimitive from "@radix-ui/react-progress";
+
+import { cn } from "@/lib/utils";
+
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> & { progressColor?: string }
 >(({ className, value = 0, progressColor = "bg-primary", ...props }, ref) => {
-
   return (
     <ProgressPrimitive.Root
       ref={ref}
-      className={cn(
-        "relative h-2 w-full overflow-hidden rounded-full",
-        progressColor,
-        "opacity-20",
-        className
-      )}
+      className={cn("relative h-2 w-full overflow-hidden rounded-full bg-primary/20", className)}
       {...props}
     >
       <ProgressPrimitive.Indicator
@@ -19,5 +20,9 @@ const Progress = React.forwardRef<
         style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
       />
     </ProgressPrimitive.Root>
-  )
-})
+  );
+});
+
+Progress.displayName = "Progress";
+
+export { Progress };
