2afe3cc600ddeb033e5ca760a2134ce04340ac94
MorlonDev261 <mikandrapp@gmail.com>
2025-03-13 18:53:03 +0300

    Update index.tsx

--- src/app/_components/Progression/index.tsx
+++ src/app/_components/Progression/index.tsx
@@ -1,22 +1,22 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { Progress } from "@/components/ui/progress"
+import * as React from "react";
+import { Progress } from "@/components/ui/progress";
 
-type ProgressProps {
+type ProgressProps = {
   className?: string;
   debut?: number;
   value: number;
   timer?: number;
-}
+};
 
-export default function Progression({ className, debut= 0, value, timer= 500 }: ProgressProps) {
-  const [progress, setProgress] = React.useState(debut)
+export default function Progression({ className, debut = 0, value, timer = 500 }: ProgressProps) {
+  const [progress, setProgress] = React.useState(debut);
 
   React.useEffect(() => {
-    const execute = setTimeout(() => setProgress(value), timer)
-    return () => clearTimeout(execute)
-  }, [])
+    const execute = setTimeout(() => setProgress(value), timer);
+    return () => clearTimeout(execute);
+  }, [value, timer]); // Ajout de `value` et `timer` comme dépendances
 
-  return <Progress value={progress} className={className} />
+  return <Progress value={progress} className={className} />;
 }
