d4f89fa8be89645e0470c5c9dbfcca09ad73435b
MorlonDev261 <mikandrapp@gmail.com>
2025-03-13 21:41:49 +0300

    Update TopList.tsx

--- src/app/(dashboard)/customers/TopList.tsx
+++ src/app/(dashboard)/customers/TopList.tsx
@@ -4,15 +4,15 @@ import Avatar from "@components/Avatar";
 import Progression from "@components/Progression";
 
 const stats = [
-  { id: 1, name: "John Doe", avatar: "/avatar/avatar1.png", deals: 104083700, isOnline: true },
-  { id: 2, name: "Jane Smith", avatar: "/avatar/avatar2.png", deals: 1000830000, isOnline: false },
+  { id: 1, name: "John Doe", avatar: "/avatar/avatar1.png", deals: 14083700, isOnline: true },
+  { id: 2, name: "Jane Smith", avatar: "/avatar/avatar2.png", deals: 100830000, isOnline: false },
   { id: 3, name: "Alice Johnson", avatar: "/avatar/avatar3.png", deals: 1040837000, isOnline: true },
-  { id: 4, name: "Bob Brown", avatar: "/avatar/avatar4.png", deals: 3480837000, isOnline: false },
+  { id: 4, name: "Bob Brown", avatar: "/avatar/avatar4.png", deals: 80837000, isOnline: false },
   { id: 5, name: "Charlie White", avatar: "/avatar/avatar5.png", deals: 1040000, isOnline: true },
   { id: 6, name: "David Green", avatar: "/avatar/avatar6.png", deals: 1040000, isOnline: false },
   { id: 7, name: "Emma Blue", avatar: "/avatar/avatar7.png", deals: 18040000, isOnline: true },
   { id: 8, name: "Frank Black", avatar: "/avatar/avatar1.png", deals: 10470000, isOnline: false },
-  { id: 9, name: "Grace Yellow", avatar: "/avatar/avatar4.png", deals: 100000000, isOnline: true },
+  { id: 9, name: "Grace Yellow", avatar: "/avatar/avatar4.png", deals: 1000000, isOnline: true },
   { id: 10, name: "Hank Gray", avatar: "/avatar/avatar6.png", deals: 9040000, isOnline: false },
 ];
 
@@ -38,10 +38,6 @@ const TopList = () => {
               key={stat.id}
               className="flex items-center p-3 rounded-lg cursor-pointer transition-colors hover:bg-gray-100 dark:hover:bg-gray-800"
             >
-              {/* Numéro */}
-              <span className={`mr-4 font-bold ${textColor} ${index < 3 ? "text-xl" : "text-base"}`}>
-                {index + 1}.
-              </span>
 
               {/* Avatar */}
               <Avatar className="mr-3" isOnline={stat.isOnline} isStory={true} src={stat.avatar} fallback={stat.name[0]} />
@@ -55,7 +51,7 @@ const TopList = () => {
                 </div>
 
                 {/* Barre de progression */}
-                <Progression value={percentage} />
+                <Progression value={percentage} className={textColor} />
               </div>
             </div>
           );
