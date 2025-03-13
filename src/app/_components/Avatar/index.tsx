ff0e48117a96a3af09fb1424ea182ffa40d143ab
MorlonDev261 <mikandrapp@gmail.com>
2025-03-13 19:23:26 +0300

    Update index.tsx

--- src/app/_components/Avatar/index.tsx
+++ src/app/_components/Avatar/index.tsx
@@ -13,12 +13,12 @@ interface ChatAvatarProps {
 
 }
 
-const ProfileAvatar = ({ src, Fallback= <FaUser />, isOnline = false, isStory = false, className, children }: ChatAvatarProps) => {
+const ProfileAvatar = ({ src, fallback= <FaUser />, isOnline = false, isStory = false, className, children }: ChatAvatarProps) => {
   return (
     <span className={cn("relative", className)}>
       <Avatar className={cn(isStory && "border-2 border-green-700")}>
         <AvatarImage className="p-1" src={src} alt="Profile" />
-        <AvatarFallback>{Fallback}</AvatarFallback>
+        <AvatarFallback>{fallback}</AvatarFallback>
       </Avatar>
       {isOnline && (
         <span className="absolute bottom-0 right-0 w-3 h-3 bg-green-500 border-2 border-white rounded-full"></span>
