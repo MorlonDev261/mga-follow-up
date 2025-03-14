6d48bf2117b59c98a661714e4ef5231c83d0ac45
MorlonDev261 <mikandrapp@gmail.com>
2025-03-14 16:47:51 +0300

    Create Transactions.tsx

---
+++ src/app/_components/Table/Transactions.tsx
@@ -0,0 +1,260 @@
+"use client"
+
+import * as React from "react"
+import { ColumnDef, ColumnFiltersState, SortingState, VisibilityState, flexRender, getCoreRowModel, getFilteredRowModel, getPaginationRowModel, getSortedRowModel, useReactTable } from "@tanstack/react-table"
+import TableFilter from "@components/Table/Config/TableFilter"
+import { ChevronDown, MoreHorizontal } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+import { cn } from "@/lib/utils"
+
+const data: Payment[] = [
+  {
+    id: "m5gr84i9",
+    date: "23/01/25",
+    comments: "Dépôt MVola Ergit",
+    amount: 316,
+    caisse: "Payment le 11"
+  },
+  {
+    id: "3u1reuv4",
+    date: "24/01/25",
+    comments: "Nividy sonnerie antragno",
+    amount: -242,
+    caisse: "Payment le 16"
+  },
+  {
+    id: "derv1ws0",
+    date: "26/01/25",
+    comments: "Échange Josepha",
+    amount: -837,
+    caisse: "Payment le 01"
+  },
+  {
+    id: "5kma53ae",
+    date: "29/01/25",
+    comments: "Google pixel 2pcs",
+    amount: 874,
+    caisse: "Payment le 06"
+  },
+  {
+    id: "bhqecj4p",
+    date: "31/01/25",
+    comments: "Volan'ny forfait",
+    amount: -21,
+    caisse: "Payment le 30"
+  },
+]
+
+export type Payment = {
+  id: string
+  date: string
+  comments: string
+  amount: number
+  caisse: string
+}
+
+export const columns: ColumnDef<Payment>[] = [
+  {
+  accessorKey: "date",
+    header: "Date",
+    cell: ({ row }) => {
+      const rawDate = new Date(row.getValue("date"));
+      const formattedDate = rawDate.toLocaleDateString("fr-FR"); // Format: "23/01/2025"
+      return <div>{formattedDate}</div>;
+    },
+  },
+  {
+    accessorKey: "comments",
+    header: "Comments",
+    cell: ({ row }) => <div className="lowercase">{row.getValue("comments")}</div>,
+  },
+  {
+    accessorKey: "amount",
+    header: () => <div className="text-right">Amount</div>,
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("amount"));
+
+      // Format the amount as a currency (USD)
+      const formatted = amount;
+
+      return (
+        <div className={cn("text-right font-medium", amount > 0 ? "text-green-500" : "text-red-500")}>
+          {amount > 0 ? "+" + formatted : formatted} Ar
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "caisse",
+    header: "Caisse",
+    cell: ({ row }) => <div className="lowercase">{row.getValue("caisse")}</div>,
+  },
+  {
+    id: "actions",
+    enableHiding: false,
+    cell: ({ row }) => {
+      const payment = row.original
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(payment.id)}
+            >
+              Copy payment ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    },
+  },
+]
+
+export default function MainTable() {
+  const [sorting, setSorting] = React.useState<SortingState>([])
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  )
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>({})
+  const [rowSelection, setRowSelection] = React.useState({})
+
+  const table = useReactTable({
+    data,
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+    },
+  })
+
+  return (
+    <div className="w-full p-2">
+      <div className="flex items-center gap-2 py-2">
+        <TableFilter table={table} />
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="ml-auto">
+              Columns <ChevronDown />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            {table
+              .getAllColumns()
+              .filter((column) => column.getCanHide())
+              .map((column) => {
+                return (
+                  <DropdownMenuCheckboxItem
+                    key={column.id}
+                    className="capitalize"
+                    checked={column.getIsVisible()}
+                    onCheckedChange={(value) =>
+                      column.toggleVisibility(!!value)
+                    }
+                  >
+                    {column.id}
+                  </DropdownMenuCheckboxItem>
+                )
+              })}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  )
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <div className="flex-1 text-sm text-muted-foreground">
+          Page {table.getState().pagination.pageIndex + 1} of {table.getPageCount()}
+        </div>
+
+        <div className="space-x-2">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
