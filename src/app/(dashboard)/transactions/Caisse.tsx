adb5f10a32b3de665ad8e960f45ebc68b964942c
MorlonDev261 <mikandrapp@gmail.com>
2025-03-15 01:16:53 +0300

    Update Caisse.tsx

--- src/app/(dashboard)/transactions/Caisse.tsx
+++ src/app/(dashboard)/transactions/Caisse.tsx
@@ -1,19 +1,11 @@
 "use client";
 
-import React, { lazy, Suspense } from "react";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 
-// Lazy Load des icônes pour optimiser le chargement
-const FaUsers = lazy(() => import("react-icons/fa").then((m) => ({ default: m.FaUsers })));
-const BsShopWindow = lazy(() => import("react-icons/bs").then((m) => ({ default: m.BsShopWindow })));
-const BsPersonWorkspace = lazy(() => import("react-icons/bs").then((m) => ({ default: m.BsPersonWorkspace })));
-const FiDollarSign = lazy(() => import("react-icons/fi").then((m) => ({ default: m.FiDollarSign })));
-const FiClock = lazy(() => import("react-icons/fi").then((m) => ({ default: m.FiClock })));
-const SwapWallet = lazy(() => import("@icons").then((m) => ({ default: m.SwapWallet }))); // Vérifie si l'import est correct
 
 // Liste des raccourcis
-const shortcuts = [
+const caisses = [
   {
     label: "Caisse 1",
     to: "?caisse=uzRt253",
@@ -55,22 +47,22 @@ const shortcuts = [
 const Caisse = () => {
   return (
     <nav aria-label="Shortcuts Navigation" className="grid grid-cols-4 md:grid-cols-8 sm:grid-cols-6 gap-3 p-2 w-full">
-      {shortcuts.map((shortcut) => (
+      {caisses.map((caisse) => (
         <Link
-          key={shortcut.label}
-          href={shortcut.to}
-          title={shortcut.label} // Ajout du title pour le SEO
-          aria-label={shortcut.label}
+          key=caisse.label}
+          href={caisse.to}
+          title={caisse.label} // Ajout du title pour le SEO
+          aria-label={caisse.label}
           className={cn(
             "flex h-20 flex-col items-center justify-center rounded shadow-md dark:shadow-none dark:bg-[#262a2e] p-2",
             "transition-all duration-300 transform hover:scale-105", // Animation de zoom
             "hover:bg-gray-100 dark:hover:bg-gray-700", // Changement de background au hover
             "bg-gradient-to-r", // Applique le gradient
-            shortcut.color
+            caisse.color
           )}
         >
-          <span className="text-xs">{shortcut.label}</span>
-          {shortcut.value !== undefined && <b className="text-xs">{shortcut.value.toLocaleString()} Ar</b>}
+          <span className="text-xs">{caisse.label}</span>
+          {caisse.value !== undefined && <b className="text-xs">{caisse.value.toLocaleString()} Ar</b>}
         </Link>
       ))}
     </nav>
