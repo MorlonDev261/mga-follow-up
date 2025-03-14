4a27cfae6f5115a79912bfb8fcd73f95e4f7a69a
MorlonDev261 <mikandrapp@gmail.com>
2025-03-14 19:37:31 +0300

    Update page.tsx

--- src/app/(dashboard)/transactions/page.tsx
+++ src/app/(dashboard)/transactions/page.tsx
@@ -1,10 +1,10 @@
 import { Suspense } from "react";
-import PendingContent from "./PendingContent";
+import TransactionContent from "./TransactionContent";
 
 export default function Page() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <PendingContent />
+      <TransactionContent />
     </Suspense>
   );
 }
