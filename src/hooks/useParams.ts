e188f2b21763cc60774efd7143557f7e13280ace
MorlonDev261 <mikandrapp@gmail.com>
2025-03-16 18:47:07 +0300

    Update useParams.ts

--- src/hooks/useParams.ts
+++ src/hooks/useParams.ts
@@ -1,13 +1,22 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export function useParams() {
   const searchParams = useSearchParams();
+  const [params, setParams] = useState<Record<string, string | null>>({});
 
-  const getParams = (key: string): string | null => {
-    return searchParams.get(key);
-  };
+  useEffect(() => {
+    const newParams: Record<string, string | null> = {};
+    searchParams.forEach((value, key) => {
+      newParams[key] = value;
+    });
+
+    setParams(newParams);
+  }, [searchParams]);
+
+  const getParams = (key: string) => params[key] || null;
 
   return { getParams };
 }
