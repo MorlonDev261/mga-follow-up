5a2e3d286d16c4ca86aa0fee37ee610298619cd8
MorlonDev261 <mikandrapp@gmail.com>
2025-03-17 21:43:15 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -77,8 +77,8 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <div className="min-h-screen">{children}</div>
-          <div className="absolute bottom-5 right-3 z-50 h-4 w-4 rounded-full p-2 bg-green-500 dark:bg-orange-500">
-            <Image src="/assistant.png" width="45" height="45" alt="Assistant MGA Follow UP" />
+          <div className="fixed bottom-10 right-5 z-100 rounded-full p-2 bg-green-500 dark:bg-orange-500">
+            <Image src="/assistant.png" width="100" height="100" alt="Assistant MGA Follow UP" />
           </div>
           <PWA />
         </ThemeProvider>
