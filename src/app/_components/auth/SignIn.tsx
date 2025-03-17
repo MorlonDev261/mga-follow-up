21569d9140d696e444759e4d3d368bc5b322cce3
MorlonDev261 <mikandrapp@gmail.com>
2025-03-17 12:22:06 +0300

    Update SignIn.tsx

--- src/app/_components/auth/SignIn.tsx
+++ src/app/_components/auth/SignIn.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState, useRef } from "react";
 import Link from "next/link";
+import "CSS/styles.css";
 import { Alert, AlertDescription } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
