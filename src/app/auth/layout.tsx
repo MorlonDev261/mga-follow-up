4f2998fc1bbbe54e9c319df02f8337f31e9c9719
MorlonDev261 <mikandrapp@gmail.com>
2025-03-17 13:41:28 +0300

    Update layout.tsx

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -4,8 +4,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <>
       <div className="flex flex-col mb-2 items-center">
-        <Image src="/logo.png" width="100" height="50" alt="Logo" />
-        <Image src="/logo-name.png" width="50" height="50" alt="Logo name" />
+        <Image src="/logo.png" width="100" height="100" alt="Logo" />
+        <Image src="/logo-name.png" width="200" height="40" alt="Logo name" />
       </div>
       {children}
     </>
