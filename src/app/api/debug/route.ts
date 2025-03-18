22c2fec01740e8a110f0fa27572789026541747c
MorlonDev261 <mikandrapp@gmail.com>
2025-03-19 00:04:23 +0300

    Update route.ts

--- src/app/api/debug/route.ts
+++ src/app/api/debug/route.ts
@@ -1,6 +1,10 @@
 export async function GET() {
   return Response.json({
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET ? "OK" : "Manquant",
+    AUTH_GITHUB_ID: process.env.AUTH_GITHUB_ID,
+    AUTH_GITHUB_SECRET: process.env.AUTH_GITHUB_SECRET,
+    AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID,
+    AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET,
   });
 }
