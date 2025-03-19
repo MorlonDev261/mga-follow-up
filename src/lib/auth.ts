4a314c0eb7d54a41a318644bff4b3f24d0ff0f66
MorlonDev261 <mikandrapp@gmail.com>
2025-03-19 12:30:22 +0300

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,14 +1,7 @@
-import NextAuth from "next-auth";
-import authConfig from "./auth.config";
-import type { NextAuthOptions } from "next-auth";
-
-const authInstance = NextAuth({
-  debug: true,
+import NextAuth from "next-auth"
+import authConfig from "./auth.config"
+ 
+export const { handlers, signIn, signOut, auth } = NextAuth({
   session: { strategy: "jwt" },
-  ...authConfig,
-} satisfies NextAuthOptions);
-
-export const handlers = authInstance.handlers;
-export const signIn = authInstance.signIn;
-export const signOut = authInstance.signOut;
-export const auth = authInstance.auth;
+  ...authConfig
+})
