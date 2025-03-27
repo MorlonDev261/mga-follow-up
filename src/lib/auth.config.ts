728e5a265d2ae101d0003b0dc86171fcf10e2359
MorlonDev261 <mikandrapp@gmail.com>
2025-03-27 15:18:30 +0300

    Update auth.config.ts

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -1,16 +1,28 @@
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { db } from "@/lib/db";
 import type { NextAuthConfig } from "next-auth";
 
 export default {
+  adapter: PrismaAdapter(db),
+  session: { strategy: "jwt" },
   providers: [
     GitHub({
-      clientId: process.env.AUTH_GITHUB_ID,
-      clientSecret: process.env.AUTH_GITHUB_SECRET
+      clientId: process.env.AUTH_GITHUB_ID!,
+      clientSecret: process.env.AUTH_GITHUB_SECRET!,
     }),
     Google({
-      clientId: process.env.AUTH_GOOGLE_ID,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET
-    })
-  ]
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id; // Ajoute l'ID de l'utilisateur à la session
+      }
+      return session;
+    },
+  },
 } satisfies NextAuthConfig;
