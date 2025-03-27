25ef7dadb463e7662161c3389ac91a8495b636ff
MorlonDev261 <mikandrapp@gmail.com>
2025-03-27 04:42:49 +0300

    Create route.ts

---
+++ src/app/api/test/route.ts
@@ -0,0 +1,13 @@
+import { signIn } from "next-auth/react";
+
+const handleLogin = async () => {
+  const result = await signIn("credentials", {
+    redirect: false,
+    contact: "test@example.com",
+    password: "motdepasse",
+  });
+
+  console.log(result); // Vérifie la réponse ici
+};
+
+<button onClick={handleLogin}>Se connecter</button>;
