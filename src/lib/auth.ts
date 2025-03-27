9c41c80695b06c05d9a230f0c4af55b94ab72aa8
MorlonDev261 <mikandrapp@gmail.com>
2025-03-27 23:27:14 +0300

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,4 @@
 import { auth } from "next-auth";
 import authConfig from "./auth.config";
 
-export const { handlers, signIn, signOut } = auth(authConfig);
+export const { handlers, signIn, signOut, auth } = auth(authConfig);
