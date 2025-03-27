b69451f2442fd16eee9a5ab70604025aca833683
MorlonDev261 <mikandrapp@gmail.com>
2025-03-27 04:59:18 +0300

    Update page.tsx

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -2,14 +2,19 @@
 
 import { signIn } from "next-auth/react";
 
-const handleLogin = async () => {
+
+
+export default async function Page() {
+  const handleLogin = async () => {
   const result = await signIn("credentials", {
     redirect: false,
-    contact: "test@example.com",
-    password: "motdepasse",
+    contact: "morlonrnd5@gmail.com",
+    password: "12345678M$",
   });
 
   console.log(result); // Vérifie la réponse ici
 };
-
-<button onClick={handleLogin}>Se connecter</button>;
+  return (
+    <button onClick={handleLogin}>Se connecter</button>;
+  );
+}
