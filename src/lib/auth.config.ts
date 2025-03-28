1d2e1ebfd4729cc6f24ebb40909bf38d4ab2fde9
MorlonDev261 <mikandrapp@gmail.com>
2025-03-28 10:28:32 +0300

    Update auth.config.ts

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -1,8 +1,7 @@
-import GitHub from "next-auth/providers/github";
-import Google from "next-auth/providers/google";
 import type { AuthOptions } from "next-auth";
 
 export const authConfig: AuthOptions = {
+  session: { strategy: "jwt" as const }, // Ajout du `as const`
   providers: [
     GitHub({
       clientId: process.env.GITHUB_CLIENT_ID!,
