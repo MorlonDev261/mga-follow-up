f1f68374215e6d1544fad9ba3290aade4b1fa3d3
MorlonDev261 <mikandrapp@gmail.com>
2025-03-28 16:24:44 +0300

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,37 @@
-import NextAuth from "next-auth";
-import authConfig from "./auth.config";
+// auth.ts
+import NextAuth from "@auth/nextjs";
+import type { AuthConfig } from "@auth/core/types";
+import GitHub from "@auth/core/providers/github";
+import Google from "@auth/core/providers/google";
 
-export const { handlers, signIn, signOut, auth } = NextAuth(
-  secret: process.env.NEXTAUTH_SECRET,
-  session: { strategy: "jwt" },
-  ...authConfig
-);
+export const authConfig: AuthConfig = {
+  secret: process.env.AUTH_SECRET,
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) token.role = user.role;
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) session.user.role = token.role;
+      return session;
+    }
+  },
+  pages: {
+    signIn: "/login",
+  },
+};
+
+export const { handlers, auth, signIn, signOut } = NextAuth(authConfig);
