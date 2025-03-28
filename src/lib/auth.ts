8e48bc14c31c3b544a62734a8526bd49b3922098
MorlonDev261 <mikandrapp@gmail.com>
2025-03-28 16:17:00 +0300

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,8 @@
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
 
-export const { handlers, signIn, signOut, auth } = NextAuth(authConfig);
+export const { handlers, signIn, signOut, auth } = NextAuth(
+  secret: process.env.NEXTAUTH_SECRET,
+  session: { strategy: "jwt" },
+  ...authConfig
+);
