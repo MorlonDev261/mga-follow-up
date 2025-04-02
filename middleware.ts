8697dcb942317b43f2978e1bb8d2d387f23c73a6
MorlonDev261 <mikandrapp@gmail.com>
2025-04-02 19:55:07 +0300

    Update and rename middleware.txt to middleware.ts

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { auth } from "./auth"
+
+export default auth((reg) => {
+  // req.auth
+})
+
+// Optionally, don't invoke Middleware on some paths
+
+export const config = {
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+}
