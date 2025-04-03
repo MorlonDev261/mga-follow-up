9502ddecfd7843a461720803749157cead803919
MorlonDev261 <mikandrapp@gmail.com>
2025-04-03 12:47:32 +0300

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,7 +3,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter"
 import db from "@/lib/db"
 import authConfig from "./auth.config"
 
-export const { handlers: { GET, POST }, auth } = NextAuth({
+export const { auth, handlers, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
