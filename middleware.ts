69d60e50e4826fdd6273b73d8ce1efad40a2f3a1
MorlonDev261 <mikandrapp@gmail.com>
2025-04-03 12:23:08 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -12,9 +12,5 @@ export default auth((req) => {
 // Optionally, don't invoke Middleware on some paths
 
 export const config = {
-  matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+  matcher:['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 }
