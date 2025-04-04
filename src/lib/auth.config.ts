eb0afed84252f83e8fd44b56c636955d2cd66c3a
MorlonDev261 <mikandrapp@gmail.com>
2025-04-04 18:40:17 +0300

    Update auth.config.ts

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -1,6 +1,6 @@
 import GitHub from "next-auth/providers/github"
-import type { NextAuthConfig } from "next-auth"
- 
-export default { 
-  providers: [GitHub] 
-} satisfies NextAuthConfig
+import type { AuthOptions } from "next-auth"
+
+export default {
+  providers: [GitHub],
+} satisfies AuthOptions
