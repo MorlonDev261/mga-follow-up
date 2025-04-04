b361b94afe17ea09138edd6d0effa01c3622aa97
MorlonDev261 <mikandrapp@gmail.com>
2025-04-04 19:39:50 +0300

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,12 @@
-import NextAuth from "next-auth/config"; // ✅ c’est ici la bonne source
-import authConfig from "./auth.config";
-
-export const {
-  handlers: { GET, POST },
-  auth,
-  signIn,
-  signOut,
-} = NextAuth(authConfig);
+import NextAuth from "next-auth"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+import authConfig from "./auth.config"
+ 
+const prisma = new PrismaClient()
+ 
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  ...authConfig,
+})
