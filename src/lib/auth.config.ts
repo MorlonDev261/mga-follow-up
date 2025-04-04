a7961e8c857b489abf2cff0642fec74967ac1ea5
MorlonDev261 <mikandrapp@gmail.com>
2025-04-04 08:13:14 +0300

    Update auth.config.ts

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -1,8 +1,6 @@
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 import Credentials from "next-auth/providers/credentials";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
 import { compare } from "bcryptjs";
 import type { NextAuthConfig } from "next-auth";
 import { z } from "zod";
