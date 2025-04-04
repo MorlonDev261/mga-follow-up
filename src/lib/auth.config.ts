d06df0ddc090ae4a225a901ef88e223ac74ca34c
MorlonDev261 <mikandrapp@gmail.com>
2025-04-04 19:40:19 +0300

    Update auth.config.ts

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -1,14 +1,4 @@
-import GitHub from "next-auth/providers/github";
-
-const authConfig = {
-  session: { strategy: "jwt" },
-  secret: process.env.AUTH_SECRET,
-  providers: [
-    GitHub({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-    }),
-  ],
-};
-
-export default authConfig;
+import GitHub from "next-auth/providers/github"
+import type { NextAuthConfig } from "next-auth"
+ 
+export default { providers: [GitHub] } satisfies NextAuthConfig
