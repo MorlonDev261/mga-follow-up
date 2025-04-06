e00c582225457aa6c9fdaff600f844cac865874c
MorlonDev261 <mikandrapp@gmail.com>
2025-04-06 19:07:20 +0300

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,64 +1,6 @@
 import NextAuth from 'next-auth'
-import CredentialsProvider from 'next-auth/providers/credentials'
-import bcrypt from 'bcryptjs'
-import prisma from '@/lib/db' // Ensure you have prisma set up
-
-export const authOptions = {
-  providers: [
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        email: { label: 'Email', type: 'email' },
-        password: { label: 'Password', type: 'password' },
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password) {
-          throw new Error('Email and password are required.')
-        }
-
-        const user = await prisma.user.findUnique({
-          where: { email: credentials.email },
-        })
-
-        if (!user) {
-          throw new Error('Invalid email or password.')
-        }
-
-        const isPasswordValid = await bcrypt.compare(credentials.password, user.password)
-        if (!isPasswordValid) {
-          throw new Error('Invalid email or password.')
-        }
-
-        return { id: user.id, email: user.email, firstName: user.firstName, lastName: user.lastName, phone: user.phone }
-      },
-    }),
-  ],
-  session: {
-    strategy: 'jwt', // Using JWT for session management
-  },
-  pages: {
-    signIn: '/auth/login', // Customize your sign-in page if necessary
-  },
-  callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        token.id = user.id
-        token.email = user.email
-        token.user.name = token.name
-      }
-      return token
-    },
-    async session({ session, token }) {
-      if (token) {
-        session.user.id = token.id
-        session.user.email = token.email
-        session.user.name = token.name
-      }
-      return session
-    },
-  },
-  secret: process.env.JWT_SECRET || 'your_secret_key', // Optional, specify a JWT secret
-}
+import { authOptions } from '@/lib/authOptions'
 
 const handler = NextAuth(authOptions)
+
 export { handler as GET, handler as POST }
