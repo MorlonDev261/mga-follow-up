86395fd880aba484356a917462738c7dafd5c8aa
MorlonDev261 <mikandrapp@gmail.com>
2025-04-06 19:54:48 +0300

    Update auth.config.ts

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -10,7 +10,7 @@ export const authConfig = {
         email: { label: 'Email', type: 'email' },
         password: { label: 'Password', type: 'password' },
       },
-      async authorize(credentials: Record<"email" | "password", string> | undefined) {
+      async authorize(credentials: Partial<Record<"email" | "password", unknown>>, _req: Request) {
         if (!credentials?.email || !credentials?.password) {
           throw new Error('Email and password are required.')
         }
