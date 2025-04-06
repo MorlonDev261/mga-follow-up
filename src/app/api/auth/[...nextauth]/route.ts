3b8ba6fbab69bbec7362960407dc8ebf2a4a8cb6
MorlonDev261 <mikandrapp@gmail.com>
2025-04-06 19:20:04 +0300

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,2 @@
-import { handler } from '@/lib/auth'
-
-export { handler as GET, handler as POST }
+import { handlers } from "@/lib/auth" // Referring to the auth.ts we just created
+export const { GET, POST } = handlers
