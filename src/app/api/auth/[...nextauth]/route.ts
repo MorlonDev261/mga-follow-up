ac51cf7259fa831420dd7568ef625edb1c0d6ed0
MorlonDev261 <mikandrapp@gmail.com>
2025-04-07 13:47:01 +0300

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,2 @@
-import NextAuth from 'next-auth'
-import { authOptions } from '@/lib/auth'
-import { NextResponse } from 'next/server'
-
-export async function GET(req: Request) {
-  const res = await NextAuth(req, authOptions)
-  return NextResponse.json(res)
-}
-
-export async function POST(req: Request) {
-  const res = await NextAuth(req, authOptions)
-  return NextResponse.json(res)
-}
+import { handler } from "@/lib/auth" // Referring to the auth.ts we just created
+export { handler as GET, handler as POST }
