2e1390605fbbfe6f32fe9b19485dc4fb80b82eef
MorlonDev261 <mikandrapp@gmail.com>
2025-04-07 11:21:57 +0300

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,5 @@
 import NextAuth from 'next-auth'
+import type { SignInCallback } from "next-auth";
 import CredentialsProvider from 'next-auth/providers/credentials'
 import GoogleProvider from 'next-auth/providers/google'
 import GitHubProvider from 'next-auth/providers/github'
