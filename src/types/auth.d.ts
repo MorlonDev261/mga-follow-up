45aa2b124efd3866647738d4fb0006e6e80c3e75
MorlonDev261 <mikandrapp@gmail.com>
2025-04-07 13:20:57 +0300

    Update auth.d.ts

--- src/types/auth.d.ts
+++ src/types/auth.d.ts
@@ -3,20 +3,27 @@
 import 'next-auth'
 import 'next-auth/jwt'
 
-declare module 'next-auth' {
+declare module "next-auth" {
   interface Session {
     user: {
       id: string
       email: string
-      name?: string | null
+      name?: string
+      image?: string
     }
   }
-}
 
-declare module 'next-auth/jwt' {
+  interface User {
+    id: string
+    email: string
+    name?: string
+    image?: string
+  }
+
   interface JWT {
     id: string
     email: string
-    name?: string | null
+    name?: string
+    image?: string
   }
 }
