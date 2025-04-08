fd8d7a9037115e7cfa97fd9d6218b973cccd2ca4
MorlonDev261 <mikandrapp@gmail.com>
2025-04-08 22:05:03 +0300

    Update and rename protect-client.ts to protect-client-page.ts

--- src/hooks/protect-client-page.ts
+++ src/hooks/protect-client-page.ts
@@ -4,7 +4,7 @@ import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
 
-export default function useProtectPage(redirectTo = '/login') {
+export default function ProtectPage(redirectTo = '/login') {
   const { status } = useSession()
   const router = useRouter()
 
