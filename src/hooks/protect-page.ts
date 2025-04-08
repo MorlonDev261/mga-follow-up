6abd71062aba42f42c7880cc64d1a9fbbe023ea6
MorlonDev261 <mikandrapp@gmail.com>
2025-04-08 21:11:40 +0300

    Update and rename next-auth.ts to protect-page.ts

--- src/hooks/protect-page.ts
+++ src/hooks/protect-page.ts
@@ -1,18 +1,16 @@
 'use client'
 
 import { useSession } from 'next-auth/react'
-import { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 
-export default function useAuth(redirectTo = '/login') {
-  const { data: session, status } = useSession()
+export default function useProtectPage(redirectTo = '/login') {
+  const { status } = useSession()
   const router = useRouter()
 
   useEffect(() => {
     if (status === 'unauthenticated') {
-      router.push(redirectTo)
+      router.replace(redirectTo)
     }
   }, [status, router, redirectTo])
-
-  return { session, status }
 }
