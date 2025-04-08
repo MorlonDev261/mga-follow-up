ddeb245ef5e47bccbe20788bc1affd66ced40343
MorlonDev261 <mikandrapp@gmail.com>
2025-04-08 16:00:16 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,12 @@
 // middleware.ts
+import { auth } from "@/auth";
 import { NextResponse } from "next/server";
-import { getToken } from "next-auth/jwt";
 
-export async function middleware(req: any) {
-  const token = await getToken({ req });
-  const isAuthenticated = !!token;
-
-  if (req.nextUrl.pathname.startsWith("/") && !isAuthenticated) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
+export async function middleware() {
+  const session = await auth();
+  if (!session) return NextResponse.redirect("/login");
 }
+
+export const config = { 
+  matcher: ["/:path*"] 
+};
