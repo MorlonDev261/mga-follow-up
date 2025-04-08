a9335431c43e6de9afff4cb581e7f974d75b8d3f
MorlonDev261 <mikandrapp@gmail.com>
2025-04-08 22:13:25 +0300

    Update page.tsx

--- src/app/(dashboard)/pending/page.tsx
+++ src/app/(dashboard)/pending/page.tsx
@@ -2,7 +2,7 @@ import { Suspense } from "react";
 import protectPage from '@/hooks/protect-server-page'
 import PendingContent from "./PendingContent";
 
-export default function Page() {
+export default async function Page() {
   await protectPage()
   return (
     <Suspense>
