b3295fbefb945a74541f68845dbb462d1d80a3a3
MorlonDev261 <mikandrapp@gmail.com>
2025-04-08 09:26:05 +0300

    Update UserAvatar.tsx

--- src/app/_components/Avatar/UserAvatar.tsx
+++ src/app/_components/Avatar/UserAvatar.tsx
@@ -1,4 +1,4 @@
-import { getServerSession } from 'next-auth';
+import getServerSession from 'next-auth';
 import { authOptions } from '@/lib/auth';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
