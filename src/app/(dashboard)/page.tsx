9b06a74347fdca41e4e43e1a129c828b760e5487
MorlonDev261 <mikandrapp@gmail.com>
2025-04-09 09:43:00 +0300

    Update page.tsx

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,13 +1,25 @@
 import HomePage from "@components/Welcome/HomePage";
 import Dashboard from "@components/Welcome/Dashboard";
+import type { Session } from "next-auth";
 import { auth } from "@/lib/auth";
 
-export default async function WelcomePage() {
-  const session = await auth();
+interface WelcomePageProps {
+  session: Session | null;
+}
 
+export default function WelcomePage({ session }: WelcomePageProps) {
   if (session?.user) {
     return <Dashboard />;
   } else {
     return <HomePage />;
   }
+}
+
+export async function getServerSideProps() {
+  const session = await auth();
+  return {
+    props: {
+      session,
+    },
+  };
 }
