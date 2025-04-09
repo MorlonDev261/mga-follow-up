47e8d304bef09ab5867b40f4cfe75634b16a4398
MorlonDev261 <mikandrapp@gmail.com>
2025-04-09 08:41:10 +0300

    Update and rename home.tsx to Dashboard.tsx

--- src/app/_components/Welcome/Dashboard.tsx
+++ src/app/_components/Welcome/Dashboard.tsx
@@ -12,7 +12,7 @@ import ShortcutList from "@components/ShortcutList";
 import Statistique from "@components/Statistique";
 import MainTable from "@components/Table/MainTable";
 
-export default function Home() {
+export default function Dashboard() {
   return (
     <>
       <div className="p-2">
