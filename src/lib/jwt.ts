75f586706af32f847067e3012358462a55f8fef4
MorlonDev261 <mikandrapp@gmail.com>
2025-04-09 10:50:23 +0300

    Update jwt.ts

--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -1,14 +1,21 @@
 import jwt from "jsonwebtoken";
 
-const secret = process.env.JWT_SECRET!; // Secret de ton token, à récupérer dans .env
+export type JwtPayload = {
+  id: string
+    email: string
+    name?: string
+    image?: string
+};
 
-export async function verifyToken(token: string) {
+const secret = process.env.JWT_SECRET!;
+
+export async function verifyToken(token: string): Promise<JwtPayload> {
   return new Promise((resolve, reject) => {
     jwt.verify(token, secret, (err, decoded) => {
-      if (err) {
+      if (err || typeof decoded !== "object" || decoded === null || !("userId" in decoded)) {
         return reject(new Error("Token invalide ou expiré"));
       }
-      resolve(decoded);
+      resolve(decoded as JwtPayload);
     });
   });
 }
