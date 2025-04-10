08ee074fb32843b32e3c0b96a7476a5e73b6c82d
MorlonDev261 <mikandrapp@gmail.com>
2025-04-10 23:24:35 +0300

    Update auth.d.ts

--- src/types/auth.d.ts
+++ src/types/auth.d.ts
@@ -9,7 +9,7 @@ declare module 'next-auth' {
       name?: string
       image?: string
       coverPicture?: string
-      emailVerified?: string | Date
+      emailVerified?: string | null
       createdAt?: string
     }
   }
