32300ddfbb9a30fe6e94166d4adb3442b0746382
MorlonDev261 <mikandrapp@gmail.com>
2025-04-11 14:45:34 +0300

    Update SignUp.tsx

--- src/app/_components/auth/SignUp.tsx
+++ src/app/_components/auth/SignUp.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import "./CSS/styles.css";
 import Link from "next/link";
