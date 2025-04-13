b7ad99aefee343ccbd27827217f83a1c9b5d35e9
MorlonDev261 <mikandrapp@gmail.com>
2025-04-13 14:26:37 +0300

    Update list.tsx

--- src/app/_components/Company/list.tsx
+++ src/app/_components/Company/list.tsx
@@ -70,7 +70,7 @@ export default function Entreprises() {
 
             <div className="max-w-[70%] p-1 overflow-hidden">
               <h3 className="flex items-center gap-[4px] text-md font-bold text-gray-800 dark:text-white">
-                <span className="line-clamp-2">
+                <span className="line-clamp-1">
                   {company.name}
                 </span>
                 {company.status === "trust" ? (
