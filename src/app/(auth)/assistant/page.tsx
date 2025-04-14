423acbdcd7fc9f964dd1e44cd7374f9ba04b63ce
MorlonDev261 <mikandrapp@gmail.com>
2025-04-14 23:36:04 +0300

    Create page.tsx

---
+++ src/app/(auth)/assistant/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Assistant from "@components/ai/assistant";
+
+const Assiss: React.FC = () => {
+
+  return (
+    <Assistant />
+  );
+};
+
+export default Assiss;
