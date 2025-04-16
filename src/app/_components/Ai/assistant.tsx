a009d501854617dfd6afbdd8fc56a35ce4e69123
MorlonDev261 <mikandrapp@gmail.com>
2025-04-16 19:06:54 +0300

    Update assistant.tsx

--- src/app/_components/Ai/assistant.tsx
+++ src/app/_components/Ai/assistant.tsx
@@ -122,17 +122,11 @@ export default function Chat() {
           "bg-gray-50 rounded-none sm:rounded-xl", // pas d’arrondi sur mobile, arrondi desktop
           "shadow-2xl overflow-hidden flex flex-col z-[999]"
         )}
-        initial={{ opacity: 0, scale: 0.95, width: '100%', height: '100%' }} // Valeurs initiales (disparu et réduit)
-        animate={{ 
-          opacity: isChatOpen ? 1 : 0, 
-          scale: isChatOpen ? 1 : 0.95, 
-          width: isChatOpen ? '100%' : '0%', // Réduit la largeur quand isChatOpen est false
-          height: isChatOpen ? '100%' : '0%', // Réduit la hauteur quand isChatOpen est false
-        }}
-        exit={{ opacity: 0, scale: 0.95, width: '0%', height: '0%' }} // Réduit également en exit
+        initial={{ opacity: 0, scale: 0.95 }}
+        animate={{ opacity: isChatOpen ? 1 : 0, scale: isChatOpen ? 1 : 0.95 }}
+        exit={{ opacity: 0, scale: 0.95 }}
         transition={{ duration: 0.3 }}
       >
-        
         {/* Header */}
         <div className="bg-white border-b border-gray-200 shadow-sm py-4 px-6">
           <div className="flex items-center justify-between">
