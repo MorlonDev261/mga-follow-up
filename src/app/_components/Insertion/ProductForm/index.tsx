5639900b5edd18396eaf0c50276fc66780ad1d1d
MorlonDev261 <mikandrapp@gmail.com>
2025-04-24 11:41:35 +0300

    Update index.tsx

--- src/app/_components/Insertion/ProductForm/index.tsx
+++ src/app/_components/Insertion/ProductForm/index.tsx
@@ -153,7 +153,8 @@ export default function ProductForm({ setOpen }: ProductFormProps) {
               { value: "remix", label: "Remix" },
               { value: "astro", label: "Astro" },
             ]}
-            setForm={setForm}
+            form
+            setForm
             showSearch={true}  // Affiche la barre de recherche
             closeOnSelect={false}  // Laisse le sélecteur ouvert après la sélection
           />
