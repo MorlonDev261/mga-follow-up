3b26c031fadfe38608ffec58938cfd94832497be
MorlonDev261 <mikandrapp@gmail.com>
2025-04-25 19:42:33 +0300

    Update index.tsx

--- src/app/_components/Uploader/index.tsx
+++ src/app/_components/Uploader/index.tsx
@@ -31,7 +31,7 @@ export default function LogoUploader({ logo, setLogo }: Props) {
       });
 
       const data = await res.json();
-      setLogo({ url: data.url, public_id: data.public_id });
+      setLogo({ url: data.secure_url, public_id: data.public_id });
     } catch (err) {
       console.error(err);
       alert("Erreur lors de l'upload du logo.");
