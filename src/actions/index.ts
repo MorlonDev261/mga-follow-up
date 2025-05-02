6dff9075e1e2daec98621074cf500d92d8f0c837
MorlonDev261 <mikandrapp@gmail.com>
2025-05-02 20:26:44 +0300

    Update index.ts

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,7 +2,7 @@
 
 import db from "@/lib/db"
 import { Role } from "@prisma/client"
-import { moment } from "moment"
+import moment from "moment"
 
 import {
   createUserSchema, updateUserSchema, CreateUserInput, UpdateUserInput,
