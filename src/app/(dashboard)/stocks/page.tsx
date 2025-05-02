b3a8b0d9d2feef3c1d42549b3022a01a8bb4c51f
MorlonDev261 <mikandrapp@gmail.com>
2025-05-02 22:33:16 +0300

    Update page.tsx

--- src/app/(dashboard)/stocks/page.tsx
+++ src/app/(dashboard)/stocks/page.tsx
@@ -1,12 +1,9 @@
 "use client"
 
-// import { Suspense } from "react";
 import StockContent from "./StockContent";
 
 export default function Page() {
   return (
-//    <Suspense>
-      <StockContent />
-//    </Suspense>
+    <StockContent />
   );
 }
