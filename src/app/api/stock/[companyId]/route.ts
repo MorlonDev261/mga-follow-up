fdde65d5cf4b726189170ed7436349a7e2a7e09d
MorlonDev261 <mikandrapp@gmail.com>
2025-05-03 17:21:28 +0300

    Update route.ts

--- src/app/api/stock/[companyId]/route.ts
+++ src/app/api/stock/[companyId]/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse, NextRequest } from 'next/server'
 import db from '@/lib/db'
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: NextRequest, res: NextResponse) {
   const { companyId } = req.query
 
   // Vérification basique
