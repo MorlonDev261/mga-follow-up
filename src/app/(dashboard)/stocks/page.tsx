05b4546faff428952aa7cec237477684e7dad886
MorlonDev261 <mikandrapp@gmail.com>
2025-05-03 07:32:37 +0300

    Update page.tsx

--- src/app/(dashboard)/stocks/page.tsx
+++ src/app/(dashboard)/stocks/page.tsx
@@ -1,11 +1,11 @@
 import { listStocksByCompany } from "@/actions";
-import StockContent from "./StockContent";
+import TransactionContent from "./StockContent";
 
 export default async function Page() {
   const companyId = "cma5mvy3i0000l504izi8zb2i";
   const stocks = await listStocksByCompany(companyId);
   
   return (
-    <StockContent stocks={stocks} />
+    <TransactionContent stocks={stocks} />
   );
 }
