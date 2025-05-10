8dd420186607322815b6123d52869964340253d3
MorlonDev261 <mikandrapp@gmail.com>
2025-05-10 09:59:14 +0300

    Update auth.d.ts

--- src/types/auth.d.ts
+++ src/types/auth.d.ts
@@ -1,57 +1,40 @@
-import 'next-auth';
-import 'next-auth/jwt';
-import { Prisma } from '@prisma/client';
-import type { DefaultSession, DefaultUser } from 'next-auth';
+import 'next-auth'
+import 'next-auth/jwt'
 
-// Définir le type Company en dehors
-type Company = {
-  id: string;
-  name: string;
-  createdAt: Date; 
-  desc: string; 
-  nif: string | null; 
-  stat: string | null; 
-  owner: string; 
-  contact: string; 
-  address: string; 
-  logo: Prisma.JsonValue | null;
-  userRole: string;
-};
-
-// Étendre les types NextAuth
 declare module 'next-auth' {
-  interface Session extends DefaultSession {
+  interface Session {
+    selectedCompany?: string
     user: {
-      id: string;
-      email: string;
-      name?: string;
-      image?: string;
-      coverPicture?: string;
-      emailVerified?: Date | null;
-      createdAt?: string;
-    };
-    company?: Company;
+      id: string
+      email: string
+      name?: string
+      image?: string
+      coverPicture?: string
+      emailVerified?: Date | null
+      createdAt?: string
+    }
   }
 
-  interface User extends DefaultUser {
-    id: string;
-    email: string;
-    name?: string;
-    image?: string;
-    coverPicture?: string;
-    emailVerified?: Date | null;
-    createdAt?: string;
+  interface User {
+    id: string
+    email: string
+    name?: string
+    image?: string
+    coverPicture?: string
+    emailVerified?: Date | null
+    createdAt?: string
   }
 }
 
 declare module 'next-auth/jwt' {
   interface JWT {
-    id: string;
-    email: string;
-    name?: string;
-    image?: string;
-    coverPicture?: string;
-    emailVerified?: Date | null;
-    createdAt?: string;
+    id: string
+    email: string
+    name?: string
+    image?: string
+    coverPicture?: string
+    emailVerified?: Date | null
+    createdAt?: string
+    selectedCompany?: string | null
   }
 }
