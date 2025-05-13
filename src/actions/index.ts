245e8b87cf68e6e238ae815a425127cd9bcb3818
MorlonDev261 <mikandrapp@gmail.com>
2025-05-13 17:39:14 +0300

    Update index.ts

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,7 +1,7 @@
 "use server"
 
 import db from "@/lib/db"
-import { Role, Prisma } from "@prisma/client"
+import { Prisma } from "@prisma/client"
 import moment from "moment"
 
 import {
@@ -13,6 +13,8 @@ import {
   customerRelationSchema, CustomerRelationInput,
   conversationSchema, ConversationInput
 } from "./schema"
+
+type Role = "OWNER" | "SUB_OWNER" | "MEMBER"
 
 // --- USERS ---
 
