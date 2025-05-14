3cfded1d8ae73f3af0d9d39f539154c73dcd7e91
MorlonDev261 <mikandrapp@gmail.com>
2025-05-14 19:43:32 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,15 @@
-import runtimeCaching from './src/lib/cache'; // ou './lib/cache' si tu l’as mis ailleurs
+import runtimeCaching from './src/lib/cache';
+
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV !== 'production',
   runtimeCaching,
-  // "buildExcludes: [/middleware-manifest\.json$/], // optionnel si tu as des erreurs de build
 });
 
 const nextConfig = {
   reactStrictMode: true,
-  trustedHosts: [
-    'mga-follow-up.com',
-    'localhost:3000',
-  ],
   images: {
     remotePatterns: [
       {
