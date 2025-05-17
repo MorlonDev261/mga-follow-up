3902a68ff400fec238f894e93252e17f44aef612
MorlonDev261 <mikandrapp@gmail.com>
2025-05-17 14:24:52 +0300

    Update page.tsx

--- src/app/(dashboard)/stocks/page.tsx
+++ src/app/(dashboard)/stocks/page.tsx
@@ -1,15 +1,12 @@
-import { listStocksByCompany } from "@/actions";
 import StockContent from "./StockContent";
 import { auth } from "@/lib/auth";
 
 export default async function Page() {
-  const session = await auth();
+ const session = await auth();
 
-  if (!session?.selectedCompany) {
+ if (!session?.selectedCompany) {
     return <div>Aucune entreprise sélectionnée</div>;
   }
 
-  const stocks = await listStocksByCompany(session.selectedCompany);
-
-  return <StockContent stocks={stocks} companyId={session.selectedCompany} />;
+  return <StockContent companyId={session.selectedCompany} />;
 }
