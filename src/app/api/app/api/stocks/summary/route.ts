640014f0b040a94b57650337ceac0d0c7015a941
MorlonDev261 <mikandrapp@gmail.com>
2025-05-17 13:58:25 +0300

    Update route.ts

--- src/app/api/app/api/stocks/summary/route.ts
+++ src/app/api/app/api/stocks/summary/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { auth } from "@/lib/auth";
-import { db } from "@/lib/db";
+import db from "@/lib/db";
 import moment from "moment";
 
 export async function GET(req: NextRequest) {
